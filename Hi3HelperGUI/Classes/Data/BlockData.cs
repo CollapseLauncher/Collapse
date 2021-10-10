835c5796d76ea342bdcc52e4262a584c9d41dd77
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2021-10-10 17:14:17 +0700

    Add handler for BlockData reading

--- Hi3HelperGUI/Classes/Data/BlockData.cs
+++ Hi3HelperGUI/Classes/Data/BlockData.cs
@@ -16,12 +16,56 @@ namespace Hi3HelperGUI.Data
     public class BlockData
     {
         XMFUtils util;
+        public event EventHandler<ReadingBlockProgressChanged> ProgressChanged;
+        public event EventHandler<ReadingBlockProgressCompleted> Completed;
         public BlockData(in MemoryStream i) {
             i.Position = 0;
             util = new XMFUtils(i, XMFFileFormat.Dictionary);
             util.Read();
         }
-        
+
+        public void CheckIntegrity()
+        {
+
+        }
+
+        protected virtual void OnProgressChanged(ReadingBlockProgressChanged e)
+        {
+            var handler = ProgressChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected virtual void OnCompleted(ReadingBlockProgressCompleted e)
+        {
+            var handler = Completed;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public class ReadingBlockProgressChanged : EventArgs
+        {
+            public ReadingBlockProgressChanged(long totalRead, long blockSize, double totalSecond)
+            {
+                BytesRead = totalRead;
+                BlockSize = blockSize;
+                ReadSpeed = (long)(totalRead / totalSecond);
+            }
+            public long CurrentRead { get; set; }
+            public long BytesRead { get; private set; }
+            public long BlockSize { get; private set; }
+            public long ReadSpeed { get; private set; }
+        }
+
+        public class ReadingBlockProgressCompleted : EventArgs
+        {
+            public bool ReadCompleted { get; set; }
+        }
+
         /*
         public BlockData(PresetConfigClasses i)
         {
