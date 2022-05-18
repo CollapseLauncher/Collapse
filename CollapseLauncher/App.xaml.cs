b3ad5ab544506bb01f6ec50390c751c6bedd870a
neon-nyan <dev.kemalsetyaa@gmail.com>
2022-05-18 18:15:26 +0700

    Init Console before and after InitAppPreset()

--- CollapseLauncher/App.xaml.cs
+++ CollapseLauncher/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Linq;
 
 using Windows.UI;
 
@@ -24,34 +25,46 @@ namespace CollapseLauncher
 
         public App()
         {
-#if PREVIEW
-            IsPreview = true;
-#endif
-            AppCurrentVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-
-            InitializeConsole(true, AppDataFolder);
-
-            InitAppPreset();
-            InitConsoleSetting();
-            WriteLog("App Started!", LogType.Scheme);
-            LogWriteLine($"Initializing...", LogType.Empty);
-            TryParseLocalizations();
-            LoadLocalization(GetAppConfigValue("AppLanguage").ToString());
-
-            InnerLauncherConfig.SystemAppTheme = new Windows.UI.ViewManagement.UISettings().GetColorValue(Windows.UI.ViewManagement.UIColorType.Background);
-            InnerLauncherConfig.CurrentAppTheme = Enum.Parse<AppThemeMode>(GetAppConfigValue("ThemeMode").ToString());
-
-            RequestedTheme = (InnerLauncherConfig.CurrentRequestedAppTheme = InnerLauncherConfig.GetAppTheme());
-
             try
             {
+#if PREVIEW
+                IsPreview = true;
+#endif
+                AppCurrentVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+                InitializeConsole(true, AppGameLogsFolder);
+                LogWriteLine($"Initializing...", LogType.Empty);
+
+                InitAppPreset();
+
+                InitializeConsole(true, AppGameLogsFolder);
+
+                InitConsoleSetting();
+                WriteLog($"App Version: {AppCurrentVersion} {(IsPreview ? "Preview" : "Stable")} Started! -> {GetVersionString()}", LogType.Scheme);
+                TryParseLocalizations();
+                LoadLocalization(GetAppConfigValue("AppLanguage").ToString());
+
+                InnerLauncherConfig.SystemAppTheme = new Windows.UI.ViewManagement.UISettings().GetColorValue(Windows.UI.ViewManagement.UIColorType.Background);
+                InnerLauncherConfig.CurrentAppTheme = Enum.Parse<AppThemeMode>(GetAppConfigValue("ThemeMode").ToString());
+
+                RequestedTheme = (InnerLauncherConfig.CurrentRequestedAppTheme = InnerLauncherConfig.GetAppTheme());
                 this.InitializeComponent();
             }
             catch (Exception ex)
             {
-                LogWriteLine($"FATAL ERROR!!!\r\n{ex}\r\n\r\nIf you're sure that this problem is unintentional, please report this problem by open an issue in https://github.com/neon-nyan/CollapseLauncher/issues", LogType.Error, true);
+                LogWriteLine($"FATAL ERROR!!!\r\n{ex}\r\n\r\nPlease report this problem by open an issue in https://github.com/neon-nyan/CollapseLauncher/issues", LogType.Error, true);
                 Console.ReadLine();
             }
+        }
+
+        public static string GetVersionString()
+        {
+            OperatingSystem osDetail = Environment.OSVersion;
+            ushort[] buildNumber = osDetail.Version.ToString().Split('.').Select(ushort.Parse).ToArray();
+            if (buildNumber[2] >= 22000)
+                return $"Windows 11 (build: {buildNumber[2]}.{buildNumber[3]})";
+            else
+                return $"Windows {buildNumber[0]} (build: {buildNumber[2]}.{buildNumber[3]})";
         }
 
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
