79f8edad39d956375a06cf0220feb155470bc91a
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-02-04 12:51:08 +0700

    Add ``ICache`` Interface

---
+++ CollapseLauncher/Classes/Interfaces/ICache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace CollapseLauncher.Interfaces
+{
+    internal interface ICache : IDisposable
+    {
+        ObservableCollection<AssetProperty<CacheAssetType>> AssetEntry { get; set; }
+        event EventHandler<TotalPerfileProgress> ProgressChanged;
+        event EventHandler<TotalPerfileStatus> StatusChanged;
+        Task<bool> StartCheckRoutine();
+        Task StartUpdateRoutine(bool showInteractivePrompt = false);
+        void CancelRoutine();
+    }
+}
