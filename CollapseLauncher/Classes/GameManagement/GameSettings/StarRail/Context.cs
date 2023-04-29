7ed79f5f1585f4fe90af37983030638c46d39322
bagusnl <dzakibagus@gmail.com>
2023-04-29 22:45:30 +0700

    Update HSR Context.cs - What in the world was in my brain then I did that?

--- CollapseLauncher/Classes/GameManagement/GameSettings/StarRail/Context.cs
+++ CollapseLauncher/Classes/GameManagement/GameSettings/StarRail/Context.cs
@@ -5,19 +5,8 @@ namespace CollapseLauncher.GameSettings.StarRail.Context
     //Modified from Honkai codes because of similarities in reg structure
 {
     [JsonSourceGenerationOptions(IncludeFields = false, GenerationMode = JsonSourceGenerationMode.Metadata, IgnoreReadOnlyFields = true, WriteIndented = false)]
-    [JsonSerializable(typeof(BGMVolume))]
-    internal partial class BGMVolume : JsonSerializerContext { }
-
-    [JsonSourceGenerationOptions(IncludeFields = false, GenerationMode = JsonSourceGenerationMode.Metadata, IgnoreReadOnlyFields = true, WriteIndented = false)]
-    [JsonSerializable(typeof(MasterVolume))]
-    internal partial class MasterVolume : JsonSerializerContext { }
-    [JsonSourceGenerationOptions(IncludeFields = false, GenerationMode = JsonSourceGenerationMode.Metadata, IgnoreReadOnlyFields = true, WriteIndented = false)]
-    [JsonSerializable(typeof(SFXVolume))]
-    internal partial class SFXVolume : JsonSerializerContext { }
-
-    [JsonSourceGenerationOptions(IncludeFields = false, GenerationMode = JsonSourceGenerationMode.Metadata, IgnoreReadOnlyFields = true, WriteIndented = false)]
-    [JsonSerializable(typeof(VOVolume))]
-    internal partial class VOVolume : JsonSerializerContext { }
+    [JsonSerializable(typeof(Model))]
+    internal partial class Model : JsonSerializerContext { }
 
 
 }
