69d3ab93ae6d98e225e8b93910d021e3415176a3
Ron Friedman <cryogen65@gmail.com>
2023-06-12 15:44:15 -0400

    typo fixes

--- CollapseLauncher/XAMLs/MainApp/Pages/HomePage.xaml.cs
+++ CollapseLauncher/XAMLs/MainApp/Pages/HomePage.xaml.cs
@@ -542,7 +542,7 @@ namespace CollapseLauncher.Pages
             {
                 IsPageUnload = true;
                 LogWriteLine($"Error while installing game {PageStatics._GameVersion.GamePreset.ZoneName}\r\n{ex}", Hi3Helper.LogType.Error, true);
-                ErrorSender.SendException(new NullReferenceException("Oops, the launcher cannot finalize the installation but don't worry, your game has been totally updated.\r\t" +
+                ErrorSender.SendException(new NullReferenceException("Collapse was not able to complete post-installation tasks, but your game has been successfully updated.\r\t" +
                     $"Please report this issue to our GitHub here: https://github.com/neon-nyan/CollapseLauncher/issues/new or come back to the launcher and make sure to use Repair Game in Game Settings button later.\r\nThrow: {ex}", ex));
             }
             catch (Exception ex)
