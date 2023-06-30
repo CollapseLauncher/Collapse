a842f79a3784b9a84febdbf2b679d2b3a90908db
Bagus Nur Listiyono <28079733+bagusnl@users.noreply.github.com>
2023-06-30 15:17:44 +0700

    Implement Genshin Impact Game Settings (#183)
    * GI GameSettings: Initial Implementation for ScreenManager
    
    * GI GameSettings: add debug line for ScreenManager Load()
    
    * GI GameSettings: Introduce locales
    
    * GI GameSettings: Initial implementation for frontend page
    
    * GI GameSettings: add override for isfullScreen
    
    * GI GameSettings: Initial implementation for GeneralData root tree
    
    * GI GameSettings: Force load for ScreenManager
    
    * GI GameSettings: Initial implementation for GraphicsData tree
    
    * GI GameSettings: Misc information
    
    * GI GameSettings: Fix page crashing
    
    * GI GameSettings: Fix namespaces names
    
    * GI GameSettings: Fix SettingScreen not saving
    
    * GI GameSettings: Implement RegistryWatcher
    
    * GI GameSettings: Fix missing GeneralData Equals
    
    * GI GameSettings: Use Debug LogType
    
    * GI GameSettings: Fix GeneralData.cs
    - Implement fallback for globalPerfData and _overrideControllerMapValueList
    - Fix some wrong Json keys
    
    * GI GameSettings: Fix GeneralData.cs
    - globalPerfData not saved properly
    
    * GI GameSettings: Fix value types for GeneralData feat. @Cryotechnic
    
    * GI GameSettings: Adjusted graphicsData key order
    
    * GI GameSettings: UI
    
    * GI GameSettings: Frontend Improvements
    - Add audio settings
    - Adjusted locales
    - Adjusted margins
    
    * GI GameSettings: also set globalPerfData
    
    * Fix StarRail Localization
    - Unsure how it got yeeted off there and still works...
    
    * GI GameSettings: Add Language Settings
    
    * GI GameSettings: Implement Exclusive Fullscreen toggle
    - Also added necessary warning for its use
    
    * Get ordered FPS list programmatically
    
    * GI GameSettings: Disable General Data dumping in Load/Save
    - Because its huuuuge
    
    * GI GameSettings: Locales Cleanup
    
    * GI GameSettings: Locales Cleanup pt. 2
    
    * GI GameSettings: Code Cleanup
    - Removed unused usings
    - Adjusted log console strings
    
    * GI GameSettings: Implement Gamma slider
    
    * GI GameSettings: Implement Gamma slider
    
    * Disable Exclusive Fullscreen on both Genshin and StarRail
    
    * GI GameSettings: Attempt to fix Gamma slider
    
    * fixed gamma behaving in reverse
    
    because ofc mhy would do that, thanks mhy <3
    
    ---------
    
    Co-authored-by: Myp3a <murza8965@gmail.com>
    Co-authored-by: Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
    Co-authored-by: Ron Friedman <cryogen65@gmail.com>
    Co-authored-by: Iskandar Montano <muscularcandy67@gmail.com>

---
+++ CollapseLauncher/Classes/GameManagement/GameSettings/Genshin/RegistryClass/ScreenManager.cs
@@ -0,0 +1,164 @@
+﻿using CollapseLauncher.GameSettings.Base;
+using CollapseLauncher.Interfaces;
+using Hi3Helper;
+using Hi3Helper.Screen;
+using Microsoft.Win32;
+using System;
+using System.Drawing;
+using static CollapseLauncher.GameSettings.Statics;
+using static Hi3Helper.Logger;
+
+namespace CollapseLauncher.GameSettings.Genshin
+{
+    internal class ScreenManager : BaseScreenSettingData, IGameSettingsValue<ScreenManager>
+    {
+        #region Fields
+        private const string _ValueNameScreenManagerWidth = "Screenmanager Resolution Width_h182942802";
+        private const string _ValueNameScreenManagerHeight = "Screenmanager Resolution Height_h2627697771";
+        private const string _ValueNameScreenManagerFullscreen = "Screenmanager Is Fullscreen mode_h3981298716";
+        private static Size currentRes = ScreenProp.currentResolution;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// This value references values from inner width and height.<br/><br/>
+        /// Range: 64 x 64 - int.MaxValue x int.MaxValue<br/>
+        /// Default: Your screen resolution
+        /// </summary>
+        public override Size sizeRes
+        {
+            get => new Size(width, height);
+            set
+            {
+                width = value.Width < 64 ? currentRes.Width : value.Width;
+                height = value.Height < 64 ? currentRes.Height : value.Height;
+            }
+        }
+
+        /// <summary>
+        /// This value references values from inner width and height as string.<br/><br/>
+        /// Range: 64 x 64 - int.MaxValue x int.MaxValue<br/>
+        /// Default: Your screen resolution
+        /// </summary>
+        public override string sizeResString
+        {
+            get => $"{width}x{height}";
+            set
+            {
+                string[] size = value.Split('x');
+                int w;
+                int h;
+                if (!int.TryParse(size[0], out w) || !int.TryParse(size[1], out h))
+                {
+                    width = currentRes.Width;
+                    height = currentRes.Height;
+                }
+                else
+                {
+                    width = w;
+                    height = h;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This defines "<c>Game Resolution</c>'s Width" combobox In-game settings -> Video.<br/><br/>
+        /// Range: 0 - int.MaxValue<br/>
+        /// Default: Your screen width
+        /// </summary>
+        public override int width { get; set; } = currentRes.Width;
+
+        /// <summary>
+        /// This defines "<c>Game Resolution</c>'s Height" combobox In-game settings -> Video.<br/><br/>
+        /// Range: 0 - int.MaxValue<br/>
+        /// Default: Your screen Height
+        /// </summary>
+        public override int height { get; set; } = currentRes.Height;
+
+        /// <summary>
+        /// This defines "<c>Fullscreen</c>" checkbox In-game settings -> Video.<br/><br/>
+        /// Range: 0 - 1
+        /// Default: 1
+        /// </summary>
+        public int fullscreen { get; set; } = 1;
+
+        public override bool isfullScreen
+        {
+            get => fullscreen switch
+            {
+                1 => true,
+                _ => false,
+            };
+            set => fullscreen = value switch
+            {
+                true => 1,
+                _ => 0,
+            };
+        }
+        #endregion
+#nullable enable
+        #region Methods
+        public static ScreenManager Load()
+        {
+            try
+            {
+                if (RegistryRoot == null) throw new NullReferenceException($"Cannot load {_ValueNameScreenManagerWidth} RegistryKey is unexpectedly not initialized!");
+                if (RegistryRoot == null) throw new NullReferenceException($"Cannot load {_ValueNameScreenManagerHeight} RegistryKey is unexpectedly not initialized!");
+                if (RegistryRoot == null) throw new NullReferenceException($"Cannot load {_ValueNameScreenManagerFullscreen} RegistryKey is unexpectedly not initialized!");
+
+                object? valueWidth = RegistryRoot.GetValue(_ValueNameScreenManagerWidth, null);
+                object? valueHeight = RegistryRoot.GetValue(_ValueNameScreenManagerHeight, null);
+                object? valueFullscreen = RegistryRoot.GetValue(_ValueNameScreenManagerFullscreen, null);
+                if (valueWidth != null && valueHeight != null && valueFullscreen != null)
+                {
+                    int width = (int)valueWidth;
+                    int height = (int)valueHeight;
+                    int fullscreen = (int)valueFullscreen;
+                    LogWriteLine($"Loaded Genshin Settings: {_ValueNameScreenManagerWidth} : {width}", LogType.Debug, true);
+                    LogWriteLine($"Loaded Genshin Settings: {_ValueNameScreenManagerHeight} : {height}", LogType.Debug, true);
+                    LogWriteLine($"Loaded Genshin Settings: {_ValueNameScreenManagerFullscreen} : {fullscreen}", LogType.Debug, true);
+                    return new ScreenManager { width = width, height = height, fullscreen = fullscreen };
+                }
+            }
+            catch (Exception ex)
+            {
+                LogWriteLine($"Failed while reading Genshin Impact ScreenManager Values\r\n{ex}", LogType.Error, true);
+            }
+
+            return new ScreenManager();
+        }
+
+        public override void Save()
+        {
+            try
+            {
+                RegistryRoot?.SetValue(_ValueNameScreenManagerFullscreen, fullscreen, RegistryValueKind.DWord);
+                LogWriteLine($"Saved Genshin Settings: {_ValueNameScreenManagerFullscreen} : {RegistryRoot.GetValue(_ValueNameScreenManagerFullscreen, null)}", LogType.Debug, true);
+
+                RegistryRoot?.SetValue(_ValueNameScreenManagerWidth, width, RegistryValueKind.DWord);
+                LogWriteLine($"Saved Genshin Settings: {_ValueNameScreenManagerWidth} : {RegistryRoot.GetValue(_ValueNameScreenManagerWidth, null)}", LogType.Debug, true);
+
+                RegistryRoot?.SetValue(_ValueNameScreenManagerHeight, height, RegistryValueKind.DWord);
+                LogWriteLine($"Saved Genshin Settings: {_ValueNameScreenManagerHeight} : {RegistryRoot.GetValue(_ValueNameScreenManagerHeight, null)}", LogType.Debug, true);
+            }
+            catch (Exception ex)
+            {
+                LogWriteLine($"Failed to save Genshin Impact ScreenManager Values!\r\n{ex}", LogType.Error, true);
+            }
+        }
+
+        public bool Equals(ScreenManager? comparedTo)
+        {
+            if (ReferenceEquals(this, comparedTo)) return true;
+            if (comparedTo == null) return false;
+
+            return comparedTo.sizeRes == this.sizeRes &&
+                comparedTo.height == this.height &&
+                comparedTo.width == this.width &&
+                comparedTo.fullscreen == this.fullscreen;
+        }
+        #endregion
+#nullable disable
+
+    }
+}
---
+++ CollapseLauncher/XAMLs/MainApp/Pages/GenshinGameSettingsPage.Ext.cs
@@ -0,0 +1,298 @@
+﻿using CollapseLauncher.GameSettings.Genshin;
+using CollapseLauncher.GameSettings.Genshin.Enums;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Drawing;
+
+namespace CollapseLauncher.Pages
+{
+    public sealed partial class GenshinGameSettingsPage : Page
+    {
+        #region GameResolution
+        public bool IsFullscreenEnabled
+        {
+            get => Settings.SettingsScreen.isfullScreen;
+            set
+            {
+                Settings.SettingsScreen.isfullScreen = value;
+                if (value)
+                {
+                    GameResolutionFullscreenExclusive.IsEnabled = !IsCustomResolutionEnabled;
+                    return;
+                }
+                GameResolutionFullscreenExclusive.IsEnabled = false;
+                GameResolutionFullscreenExclusive.IsChecked = false;
+            }
+        }
+
+        public bool IsCustomResolutionEnabled
+        {
+            get => Settings.SettingsCollapseScreen.UseCustomResolution;
+            set
+            {
+                Settings.SettingsCollapseScreen.UseCustomResolution = value;
+                if (value)
+                {
+                    GameResolutionFullscreenExclusive.IsEnabled = false;
+                    GameResolutionSelector.IsEnabled = false;
+                    GameCustomResolutionWidth.IsEnabled = true;
+                    GameCustomResolutionHeight.IsEnabled = true;
+
+                    string[] _size = ResolutionSelected.Split('x');
+                    GameCustomResolutionWidth.Value = int.Parse(_size[0]);
+                    GameCustomResolutionHeight.Value = int.Parse(_size[1]);
+
+                    return;
+                }
+                GameCustomResolutionWidth.IsEnabled = false;
+                GameCustomResolutionHeight.IsEnabled = false;
+                GameResolutionFullscreenExclusive.IsEnabled = IsFullscreenEnabled;
+                GameResolutionSelector.IsEnabled = true;
+
+                Size size = Hi3Helper.Screen.ScreenProp.GetScreenSize();
+                GameResolutionSelector.SelectedItem = $"{size.Width}x{size.Height}";
+            }
+        }
+
+        public bool IsCanExclusiveFullscreen
+        {
+            get => !(!IsFullscreenEnabled || IsCustomResolutionEnabled);
+        }
+
+        public bool IsExclusiveFullscreenEnabled
+        {
+            get
+            {
+                if (!IsFullscreenEnabled)
+                {
+                    return false;
+                }
+                return Settings.SettingsCollapseScreen.UseExclusiveFullscreen;
+            }
+            set
+            {
+                Settings.SettingsCollapseScreen.UseExclusiveFullscreen = value;
+                if (value)
+                {
+                    GameCustomResolutionCheckbox.IsEnabled = false;
+                    GameCustomResolutionCheckbox.IsChecked = false;
+                    GameCustomResolutionWidth.IsEnabled = false;
+                    GameCustomResolutionHeight.IsEnabled = false;
+                    return;
+                }
+                GameCustomResolutionCheckbox.IsEnabled = true;
+            }
+        }
+
+        public int ResolutionW
+        {
+            get => Settings.SettingsScreen.sizeRes.Width;
+            set => Settings.SettingsScreen.sizeRes = new Size(value, ResolutionH);
+        }
+
+        public int ResolutionH
+        {
+            get => Settings.SettingsScreen.sizeRes.Height;
+            set => Settings.SettingsScreen.sizeRes = new Size(ResolutionW, value);
+        }
+
+        public bool IsCanResolutionWH
+        {
+            get => IsCustomResolutionEnabled;
+        }
+
+        public string ResolutionSelected
+        {
+            get
+            {
+                string res = Settings.SettingsScreen.sizeResString;
+                if (string.IsNullOrEmpty(res))
+                {
+                    Size size = Hi3Helper.Screen.ScreenProp.GetScreenSize();
+                    return $"{size.Width}x{size.Height}";
+                }
+                return res;
+            }
+            set => Settings.SettingsScreen.sizeResString = value;
+        }
+
+        #endregion
+
+        #region Graphics Settings
+        public double Gamma
+        {
+            get => (double)Math.Round(Settings.SettingsGeneralData.gammaValue*-1+4.4, 5);
+            set => Settings.SettingsGeneralData.gammaValue = Math.Round(value*-1+4.4, 5);  
+            // This should belong in programmer horror stories, *DO NOT EVER DO THIS*
+            // Basically, calculate the stepper function which amounts to y = -x + 4.4, so we inverse the value and add 4.4
+            // DON'T ASK HOW WE DID THIS, IT'S a 4AM THING :)
+            // Round it to x.xxxxx because floating point
+        }
+
+        public bool VerticalSync
+        {
+            get => Convert.ToBoolean((int)Settings.SettingsGeneralData.graphicsData.VerticalSync - 1);
+            set => Settings.SettingsGeneralData.graphicsData.VerticalSync = (VerticalSyncOption)(Convert.ToInt32(value) + 1);
+        }
+
+        public bool VolumetricFog
+        {
+            get => Convert.ToBoolean((int)Settings.SettingsGeneralData.graphicsData.VolumetricFog - 1);
+            set => Settings.SettingsGeneralData.graphicsData.VolumetricFog = (VolumetricFogOption)(Convert.ToInt32(value) + 1);
+        }
+
+        public bool Reflections
+        {
+            get => Convert.ToBoolean((int)Settings.SettingsGeneralData.graphicsData.Reflections - 1);
+            set => Settings.SettingsGeneralData.graphicsData.Reflections = (ReflectionsOption)(Convert.ToInt32(value) + 1);
+        }
+
+        public bool Bloom
+        {
+            get => Convert.ToBoolean((int)Settings.SettingsGeneralData.graphicsData.Bloom - 1);
+            set => Settings.SettingsGeneralData.graphicsData.Bloom = (BloomOption)(Convert.ToInt32(value) + 1);
+        }
+
+        public int FPS
+        {
+            get
+            {
+                // Get the current value
+                FPSOption curValue = Settings.SettingsGeneralData.graphicsData.FPS;
+                // Get the index of the current value in FPSOptionsList array
+                int indexOfValue = Array.IndexOf(GraphicsData.FPSOptionsList, curValue);
+                // Return the index of the value
+                return indexOfValue;
+            }
+            set
+            {
+                // [HACK]: Fix some rare occasion where the "value" turned into -1. If so, then return
+                if (value < 0) return;
+
+                // Get the FPSOption based on the selected index by the "value"
+                FPSOption valueFromIndex = GraphicsData.FPSOptionsList[value];
+                // Set the actual value to its property
+                Settings.SettingsGeneralData.graphicsData.FPS = valueFromIndex;
+            }
+        }
+
+        public int RenderScale
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.RenderResolution - 1;
+            set => Settings.SettingsGeneralData.graphicsData.RenderResolution = (RenderResolutionOption)(value + 1);
+        }
+
+        public int ShadowQuality
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.ShadowQuality - 1;
+            set => Settings.SettingsGeneralData.graphicsData.ShadowQuality = (ShadowQualityOption)(value + 1);
+        }
+
+        public int VisualEffects
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.VisualEffects - 1;
+            set => Settings.SettingsGeneralData.graphicsData.VisualEffects = (VisualEffectsOption)(value + 1);
+        }
+
+        public int SFXQuality
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.SFXQuality - 1;
+            set => Settings.SettingsGeneralData.graphicsData.SFXQuality = (SFXQualityOption)(value + 1);
+        }
+
+        public int EnvironmentDetail
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.EnvironmentDetail - 1;
+            set => Settings.SettingsGeneralData.graphicsData.EnvironmentDetail = (EnvironmentDetailOption)(value + 1);
+        }
+
+        public int MotionBlur
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.MotionBlur - 1;
+            set => Settings.SettingsGeneralData.graphicsData.MotionBlur = (MotionBlurOption)(value + 1);
+        }
+
+        public int CrowdDensity
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.CrowdDensity - 1;
+            set => Settings.SettingsGeneralData.graphicsData.CrowdDensity = (CrowdDensityOption)(value + 1);
+        }
+
+        public int SubsurfaceScattering
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.SubsurfaceScattering - 1;
+            set => Settings.SettingsGeneralData.graphicsData.SubsurfaceScattering = (SubsurfaceScatteringOption)(value + 1);
+        }
+
+        public int CoOpTeammateEffects
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.CoOpTeammateEffects - 1;
+            set => Settings.SettingsGeneralData.graphicsData.CoOpTeammateEffects = (CoOpTeammateEffectsOption)(value + 1);
+        }
+
+        public int AnisotropicFiltering
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.AnisotropicFiltering - 1;
+            set => Settings.SettingsGeneralData.graphicsData.AnisotropicFiltering = (AnisotropicFilteringOption)(value + 1);
+        }
+
+        public int Antialiasing
+        {
+            get => (int)Settings.SettingsGeneralData.graphicsData.Antialiasing - 1;
+            set => Settings.SettingsGeneralData.graphicsData.Antialiasing = (AntialiasingOption)(value + 1);
+        }
+        #endregion
+
+        #region Audio
+        public int Audio_Global
+        {
+            get => (int)Settings.SettingsGeneralData.volumeGlobal;
+            set => Settings.SettingsGeneralData.volumeGlobal = value;
+        }
+
+        public int Audio_SFX
+        {
+            get => (int)Settings.SettingsGeneralData.volumeSFX;
+            set => Settings.SettingsGeneralData.volumeSFX = value;
+        }
+
+        public int Audio_Music
+        {
+            get => (int)Settings.SettingsGeneralData.volumeMusic;
+            set => Settings.SettingsGeneralData.volumeMusic = value;
+        }
+
+        public int Audio_Voice
+        {
+            get => (int)Settings.SettingsGeneralData.volumeVoice;
+            set => Settings.SettingsGeneralData.volumeVoice = value;
+        }
+
+        public bool Audio_DynamicRange
+        {
+            get => !Convert.ToBoolean((int)Settings.SettingsGeneralData.audioDynamicRange);
+            set => Settings.SettingsGeneralData.audioDynamicRange = Convert.ToInt32(!value);
+        }
+
+        public bool Audio_Surround
+        {
+            get => Convert.ToBoolean((int)Settings.SettingsGeneralData.audioOutput);
+            set => Settings.SettingsGeneralData.audioOutput = Convert.ToInt32(value);
+        }
+        #endregion
+
+        #region Language
+        public int AudioLang
+        {
+            get => (int)Settings.SettingsGeneralData.deviceVoiceLanguageType;
+            set => Settings.SettingsGeneralData.deviceVoiceLanguageType = value;
+        }
+
+        public int TextLang
+        {
+            get => (int)Settings.SettingsGeneralData.deviceLanguageType - 1;
+            set => Settings.SettingsGeneralData.deviceLanguageType = value + 1;
+        }
+        #endregion
+    }
+}
