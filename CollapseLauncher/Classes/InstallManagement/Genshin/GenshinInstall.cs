c387dc0d4d4ea5f0eec957fc64ea97d6aff21f6a
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-08-06 01:40:22 +0700

    Throw on unsupported GI region instead of returning

--- CollapseLauncher/Classes/InstallManagement/Genshin/GenshinInstall.cs
+++ CollapseLauncher/Classes/InstallManagement/Genshin/GenshinInstall.cs
@@ -284,14 +284,8 @@ namespace CollapseLauncher.InstallManager.Genshin
             {
                 "Global" => "GenshinImpact",
                 "Mainland China" => "YuanShen",
-                _ => string.Empty
+                _ => throw new NotSupportedException($"Unknown GI Game Region!: {_gameVersionManager.GamePreset.ZoneName}")
             };
-
-            if (string.IsNullOrEmpty(execName))
-            {
-                LogWriteLine($"Unknown GI Game Region!: {_gameVersionManager.GamePreset.ZoneName}", LogType.Error, true);
-                return new UninstallGameProperty();
-            }
 
             return new UninstallGameProperty()
             {
