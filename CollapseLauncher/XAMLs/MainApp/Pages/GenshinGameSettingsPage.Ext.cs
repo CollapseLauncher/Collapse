bf1753d4b56b9d3abcd0350e7d7e6c7ef7d912ce
Bagus Nur Listiyono <dzakibagus@gmail.com>
2023-08-17 21:43:32 +0700

    Disable Volumetric Fog when Shadow Quality is lower than Medium

--- CollapseLauncher/XAMLs/MainApp/Pages/GenshinGameSettingsPage.Ext.cs
+++ CollapseLauncher/XAMLs/MainApp/Pages/GenshinGameSettingsPage.Ext.cs
@@ -201,8 +201,31 @@ namespace CollapseLauncher.Pages
 
         public int ShadowQuality
         {
-            get => (int)Settings.SettingsGeneralData.graphicsData.ShadowQuality - 1;
-            set => Settings.SettingsGeneralData.graphicsData.ShadowQuality = (ShadowQualityOption)(value + 1);
+            get
+            {
+                int curValue = (int)Settings.SettingsGeneralData.graphicsData.ShadowQuality - 1;
+
+                // Disable Volumetric Fog when ShadowQuality is not Medium or higher
+                if (curValue < 2)
+                {
+                    VolumetricFogToggle.IsChecked = false;
+                    VolumetricFogToggle.IsEnabled = false;
+                }
+                else VolumetricFogToggle.IsEnabled = true;
+
+                return curValue;
+            }
+            set
+            {
+                if (value < 2)
+                {
+                    VolumetricFogToggle.IsChecked = false;
+                    VolumetricFogToggle.IsEnabled = false;
+                }
+                else VolumetricFogToggle.IsEnabled = true;
+
+                Settings.SettingsGeneralData.graphicsData.ShadowQuality = (ShadowQualityOption)(value + 1);
+            } 
         }
 
         public int VisualEffects
