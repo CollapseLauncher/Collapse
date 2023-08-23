a807113efa5a1f2d52563ca3d187d5f99acf3c15
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-08-23 01:11:14 +0100

    Create LangKeyboardShortcuts.cs

---
+++ Hi3Helper.Core/Lang/Locale/LangKeyboardShortcuts.cs
@@ -0,0 +1,16 @@
+﻿namespace Hi3Helper
+{
+    public sealed partial class Locale
+    {
+        #region Keyboard Shortcuts
+        public sealed partial class LocalizationParams
+        {
+            public LangKeyboardShortcuts _KbShortcuts { get; set; } = LangFallback?._KbShortcuts;
+            public sealed class LangKeyboardShortcuts
+            {
+
+            }
+        }
+        #endregion
+    }
+}
