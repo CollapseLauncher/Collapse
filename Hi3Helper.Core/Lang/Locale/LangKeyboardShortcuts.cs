68d6ce19d2adc0e64f2da7c9c2cb1b79b1467c94
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2023-08-23 14:12:33 +0100

    Update LangKeyboardShortcuts.cs

--- Hi3Helper.Core/Lang/Locale/LangKeyboardShortcuts.cs
+++ Hi3Helper.Core/Lang/Locale/LangKeyboardShortcuts.cs
@@ -8,7 +8,16 @@
             public LangKeyboardShortcuts _KbShortcuts { get; set; } = LangFallback?._KbShortcuts;
             public sealed class LangKeyboardShortcuts
             {
+                public string DialogTitle { get; set; } = LangFallback?._KbShortcuts.DialogTitle;
 
+                public string GeneralTab { get; set; } = LangFallback?._KbShortcuts.GeneralTab;
+                public string SwitchTab { get; set; } = LangFallback?._KbShortcuts.SwitchTab;
+                public string GameFolderTab { get; set; } = LangFallback?._KbShortcuts.GameFolderTab;
+                public string GameManagementTab { get; set; } = LangFallback?._KbShortcuts.GameManagementTab;
+
+                public string General_Title { get; set; } = LangFallback?._KbShortcuts.General_Title;
+                public string General_OpenMenu { get; set; } = LangFallback?._KbShortcuts.General_OpenMenu;
+                public string General_GoHome { get; set; } = LangFallback?._KbShortcuts.General_GoHome;
             }
         }
         #endregion
