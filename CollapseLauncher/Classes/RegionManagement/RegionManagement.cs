CollapseLauncher/Collapse

commit 68dad6035b02aa8584ebee68c141956c6df7c322

Disable the change game button when we make no changes (#260)

CollapseLauncher/Classes/RegionManagement/RegionManagement.cs
@@ -574,6 +574,8 @@ namespace CollapseLauncher
         private async void ChangeRegionNoWarning(object sender, RoutedEventArgs e)
         {
             (sender as Button).IsEnabled = false;
+            CurrentGameCategory = ComboBoxGameCategory.SelectedIndex;
+            CurrentGameRegion = ComboBoxGameRegion.SelectedIndex;
             await LoadRegionRootButton();
             HideLoadingPopup(true, Lang._MainPage.RegionLoadingTitle, RegionToChangeName);
             MainFrameChanger.ChangeMainFrame(m_appMode == AppMode.Hi3CacheUpdater ? typeof(CachesPage) : typeof(HomePage));
@@ -588,6 +590,8 @@ namespace CollapseLauncher
             {
                 // Finalize loading
                 ToggleChangeRegionBtn(sender, false);
+                CurrentGameCategory = ComboBoxGameCategory.SelectedIndex;
+                CurrentGameRegion = ComboBoxGameRegion.SelectedIndex;
             }
         }
 

CollapseLauncher/XAMLs/MainApp/MainPage.xaml.cs
@@ -34,6 +34,8 @@ namespace CollapseLauncher
     {
         private bool LockRegionChangeBtn;
         private bool IsLoadFrameCompleted = true;
+        private int CurrentGameCategory = -1;
+        private int CurrentGameRegion = -1;
         public static bool IsChangeDragArea = true;
         public static List<string> PreviousTagString = new List<string>();
 
@@ -776,6 +778,8 @@ namespace CollapseLauncher
 
             ComboBoxGameCategory.SelectedIndex = IndexCategory;
             ComboBoxGameRegion.SelectedIndex = IndexRegion;
+            CurrentGameCategory = ComboBoxGameCategory.SelectedIndex;
+            CurrentGameRegion = ComboBoxGameRegion.SelectedIndex;
             return LoadCurrentConfigV2((string)ComboBoxGameCategory.SelectedValue, GetComboBoxGameRegionValue(ComboBoxGameRegion.SelectedValue));
         }
 
@@ -1044,6 +1048,13 @@ namespace CollapseLauncher
 
         private void EnableRegionChangeButton(object sender, SelectionChangedEventArgs e)
         {
+            if (ComboBoxGameCategory.SelectedIndex == CurrentGameCategory && ComboBoxGameRegion.SelectedIndex == CurrentGameRegion)
+            {
+                ChangeRegionConfirmBtn.IsEnabled = false;
+                ChangeRegionConfirmBtnNoWarning.IsEnabled = false;
+                return;
+            }
+
             object selValue = ((ComboBox)sender).SelectedValue;
             if (selValue != null)
             {

commit b9da055847054ce1257870e4e96117ca12612148

Fix cannot change back region
Caused by commit 68dad60 being partially not applied to stable branch possibly due to merge error.

CollapseLauncher/Classes/RegionManagement/RegionManagement.cs
@@ -530,6 +530,8 @@ namespace CollapseLauncher
         private async void ChangeRegionNoWarning(object sender, RoutedEventArgs e)
         {
             (sender as Button).IsEnabled = false;
+            CurrentGameCategory = ComboBoxGameCategory.SelectedIndex;
+            CurrentGameRegion = ComboBoxGameRegion.SelectedIndex;
             await LoadRegionRootButton();
             HideLoadingPopup(true, Lang._MainPage.RegionLoadingTitle, RegionToChangeName);
             MainFrameChanger.ChangeMainFrame(m_appMode == AppMode.Hi3CacheUpdater ? typeof(CachesPage) : typeof(HomePage));
@@ -544,6 +546,8 @@ namespace CollapseLauncher
             {
                 // Finalize loading
                 ToggleChangeRegionBtn(sender, false);
+                CurrentGameCategory = ComboBoxGameCategory.SelectedIndex;
+                CurrentGameRegion = ComboBoxGameRegion.SelectedIndex;
             }
         }
 