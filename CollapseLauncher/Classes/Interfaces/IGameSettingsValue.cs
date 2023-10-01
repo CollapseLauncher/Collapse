f77b6e1434a08e56cce1957110841655966afed7
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2023-10-01 14:45:19 +0700

    Fix build for .NET 6

--- CollapseLauncher/Classes/Interfaces/IGameSettingsValue.cs
+++ CollapseLauncher/Classes/Interfaces/IGameSettingsValue.cs
@@ -4,7 +4,9 @@ namespace CollapseLauncher.Interfaces
 {
     internal interface IGameSettingsValue<T> : IEquatable<T>
     {
+#if NET7_0_OR_GREATER
         abstract static T Load();
+#endif
         void Save();
     }
 }
