a4cd1ca4dfa14e1b828ad32efef0d0b4b8b12169
Shatyuka <958182453@qq.com>
2023-12-17 11:30:33 +0800

    Translate the startup language select page (#336)
    * Translate the startup language select page
    
    * Revert to u2006 for existing keys
    
    * Other changes
    
    * Cleanup usings

--- CollapseLauncher/XAMLs/StartUp/StartupLanguageSelect.xaml.cs
+++ CollapseLauncher/XAMLs/StartUp/StartupLanguageSelect.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using static CollapseLauncher.InnerLauncherConfig;
 using static CollapseLauncher.WindowSize.WindowSize;
@@ -31,12 +32,79 @@ namespace CollapseLauncher
             }
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private static Dictionary<string, string> LangFallbackDict = new()
         {
-            string LangID = LanguageIDIndex[(sender as ComboBox).SelectedIndex];
-            SetAppConfigValue("AppLanguage", LangID);
-            LoadLocale(LangID);
-            NextBtn.IsEnabled = true;
+            { "en", "en-us" },
+            { "zh", "zh-cn" },
+            { "de", "de-de" },
+            { "es", "es-419" },
+            { "id", "id-id" },
+            { "ja", "ja-jp" },
+            { "ko", "ko-kr" },
+            { "pl", "pl-pl" },
+            { "pt", "pt-br" },
+            { "ru", "ru-ru" },
+            { "th", "th-th" },
+            { "vi", "vi-vn" },
+        };
+
+        private string LanguageFallback(string tag)
+        {
+            tag = tag.ToLower();
+            // Traditional Chinese
+            if (tag == "zh-hant" || tag == "zh-hk" || tag == "zh-mo" || tag == "zh-tw") return "zh-tw";
+            // Portuguese, Portugal
+            if (tag == "pt-pt") return "pt-pt";
+            if (tag.Length < 2) return "en-us";
+            tag = tag.Substring(0, 2);
+            return LangFallbackDict.GetValueOrDefault(tag, "en-us");
+        }
+
+        private void RefreshSelection()
+        {
+            SelectLang.SelectedIndex = -1;
+            SelectLang.SelectedIndex = SelectedLangIndex;
+            SelectWindowSize.SelectedIndex = -1;
+            SelectWindowSize.SelectedIndex = SelectedWindowSizeProfile;
+            SelectCDN.UpdateLayout();
+            SelectCDN.SelectedIndex = SelectedCDN;
+        }
+
+        private int SelectedLangIndex
+        {
+            get
+            {
+                var langID = GetAppConfigValue("AppLanguage").ToString() ?? LanguageFallback(CultureInfo.InstalledUICulture.Name);
+                return LanguageNames[langID.ToLower()].LangIndex;
+            }
+            set
+            {
+                if (value < 0) return;
+                var langID = LanguageIDIndex[value].ToLower();
+                if (langID == GetAppConfigValue("AppLanguage").ToString().ToLower()) return;
+                SetAppConfigValue("AppLanguage", langID);
+                LoadLocale(langID);
+
+                // Update the view
+                LogWriteLine("Updating the view...");
+                Bindings.Update();
+                LogWriteLine("Update bindings done.");
+                RefreshSelection();
+                LogWriteLine("Refresh controls done.");
+            }
+        }
+
+        private void SelectLang_OnDropDownOpened(object sender, object e)
+        {
+            // The dropdown panel collides with non-client area, making the first item not clickable.
+            // Use this to disable whole non-client area temporarily.
+            (m_window as MainWindow).DisableNonClientArea();
+        }
+
+        private void SelectLang_OnDropDownClosed(object sender, object e)
+        {
+            // And restore the nc area to normal state.
+            (m_window as MainWindow).EnableNonClientArea();
         }
 
         private IEnumerable<string> LangList
@@ -55,6 +123,7 @@ namespace CollapseLauncher
             }
             set
             {
+                if (value < 0) return;
                 CurrentWindowSizeName = WindowSizeProfilesKey[value];
             }
         }
