ec04ecca4c6413f822a2f9a7c16c3222fbf84b85
Shatyuka <958182453@qq.com>
2023-12-19 00:15:31 +0800

    Dispose the stream before deleting install package

--- CollapseLauncher/Classes/InstallManagement/BaseClass/InstallManagerBase.cs
+++ CollapseLauncher/Classes/InstallManagement/BaseClass/InstallManagerBase.cs
@@ -539,6 +539,9 @@ namespace CollapseLauncher.InstallManager.Base
                             deleteList.MoveTo(Path.Combine(_gamePath, $"deletefiles_{Path.GetFileNameWithoutExtension(asset.PathOutput)}.txt"), true);
                         }
 
+                        // Make sure that the Stream is getting disposed first
+                        stream?.Dispose();
+
                         // If the _canDeleteZip flag is true, then delete the zip
                         if (_canDeleteZip)
                         {
