a8f33607d6a47dffd54f3e4b50abf1e34f92c510
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2024-01-07 04:27:33 +0700

    Shares LangUpdatePage with ApplyUpdate's translation

--- Hi3Helper.Core/Lang/Locale/LangUpdatePage.cs
+++ Hi3Helper.Core/Lang/Locale/LangUpdatePage.cs
@@ -37,6 +37,51 @@
                 public string UpdateCountdownMessage1 { get; set; } = LangFallback?._UpdatePage.UpdateCountdownMessage1;
                 public string UpdateCountdownMessage2 { get; set; } = LangFallback?._UpdatePage.UpdateCountdownMessage2;
                 public string UpdateCountdownMessage3 { get; set; } = LangFallback?._UpdatePage.UpdateCountdownMessage3;
+
+                public string ApplyUpdateTitle1 { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTitle1;
+                public string ApplyUpdateTitle2 { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTitle2;
+                public string ApplyUpdateCDNSelectorTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateCDNSelectorTitle;
+                public string ApplyUpdateCDNSelectorSubtitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateCDNSelectorSubtitle;
+                public string ApplyUpdateCDNSelectorSubtitleCount { get; set; } = LangFallback?._UpdatePage.ApplyUpdateCDNSelectorSubtitleCount;
+                public string ApplyUpdateUpdateNowBtn { get; set; } = LangFallback?._UpdatePage.ApplyUpdateUpdateNowBtn;
+
+                public string ApplyUpdateUpdateVersionTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateUpdateVersionTitle;
+                public string ApplyUpdateUpdateChannelTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateUpdateChannelTitle;
+                public string ApplyUpdateUpdateChannelSubtitlePlaceholder { get; set; } = LangFallback?._UpdatePage.ApplyUpdateUpdateChannelSubtitlePlaceholder;
+                public string ApplyUpdateUpdateStatusTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateUpdateStatusTitle;
+                public string ApplyUpdateMiscIdle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateMiscIdle;
+                public string ApplyUpdateMiscNone { get; set; } = LangFallback?._UpdatePage.ApplyUpdateMiscNone;
+                public string ApplyUpdateVersionSeparator { get; set; } = LangFallback?._UpdatePage.ApplyUpdateVersionSeparator;
+
+                public string ApplyUpdateErrCollapseRunTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateErrCollapseRunTitle;
+                public string ApplyUpdateErrCollapseRunSubtitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateErrCollapseRunSubtitle;
+                public string ApplyUpdateErrReleaseFileNotFoundTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateErrReleaseFileNotFoundTitle;
+                public string ApplyUpdateErrReleaseFileNotFoundSubtitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateErrReleaseFileNotFoundSubtitle;
+
+                public string ApplyUpdateDownloadSizePlaceholder { get; set; } = LangFallback?._UpdatePage.ApplyUpdateMiscIdle;
+                public string ApplyUpdateDownloadSpeed { get; set; } = LangFallback?._UpdatePage.ApplyUpdateDownloadSpeed;
+                public string ApplyUpdateDownloadSpeedPlaceholder { get; set; } = LangFallback?._UpdatePage.ApplyUpdateDownloadSpeedPlaceholder;
+                public string ApplyUpdateDownloadTimeEst { get; set; } = LangFallback?._UpdatePage.ApplyUpdateDownloadTimeEst;
+                public string ApplyUpdateDownloadTimeEstPlaceholder { get; set; } = LangFallback?._UpdatePage.ApplyUpdateDownloadTimeEstPlaceholder;
+                
+                public string ApplyUpdateTaskLegacyVerFoundTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLegacyVerFoundTitle;
+                public string ApplyUpdateTaskLegacyVerFoundSubtitle1 { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLegacyVerFoundSubtitle1;
+                public string ApplyUpdateTaskLegacyVerFoundSubtitle2 { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLegacyVerFoundSubtitle2;
+                public string ApplyUpdateTaskLegacyVerFoundSubtitle3 { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLegacyVerFoundSubtitle3;
+                public string ApplyUpdateTaskLegacyVerFoundSubtitle4 { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLegacyVerFoundSubtitle4;
+                public string ApplyUpdateTaskLegacyVerFoundSubtitle5 { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLegacyVerFoundSubtitle5;
+                public string ApplyUpdateTaskLegacyCleanupCount { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLegacyCleanupCount;
+                public string ApplyUpdateTaskLegacyDeleting { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLegacyDeleting;
+
+                public string ApplyUpdateTaskDownloadingPkgTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskDownloadingPkgTitle;
+                public string ApplyUpdateTaskExtractingPkgTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskExtractingPkgTitle;
+                public string ApplyUpdateTaskRemoveOldPkgTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskRemoveOldPkgTitle;
+                public string ApplyUpdateTaskMovingExtractFileTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskMovingExtractFileTitle;
+
+                public string ApplyUpdateTaskLauncherUpdatedTitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLauncherUpdatedTitle;
+                public string ApplyUpdateTaskLauncherUpdatedSubtitle { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskLauncherUpdatedSubtitle;
+
+                public string ApplyUpdateTaskError { get; set; } = LangFallback?._UpdatePage.ApplyUpdateTaskError;
             }
         }
         #endregion
