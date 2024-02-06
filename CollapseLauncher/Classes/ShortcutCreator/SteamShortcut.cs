1c46b213b5415c9f38786a6ba62d326e95595233
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-02-06 01:32:03 +0000

    Copy Game Icon to Steam grid folder

--- CollapseLauncher/Classes/ShortcutCreator/SteamShortcut.cs
+++ CollapseLauncher/Classes/ShortcutCreator/SteamShortcut.cs
@@ -54,13 +54,6 @@ namespace CollapseLauncher.ShortcutUtils
             var id = BitConverter.GetBytes(GenerateAppId(Exe, AppName));
             appid = SteamShortcutParser.ANSI.GetString(id, 0, id.Length);
 
-            icon = Path.Combine(Path.GetDirectoryName(AppExecutablePath), "Assets\\Images\\GameIcon\\" + preset.GameType switch
-            {
-                GameType.StarRail => "icon-starrail.ico",
-                GameType.Genshin => "icon-genshin.ico",
-                _ => "icon-honkai.ico",
-            });
-
             preliminaryAppID = GeneratePreliminaryId(Exe, AppName).ToString();
 
             LaunchOptions = string.Format("open -g \"{0}\" -r \"{1}\"", preset.GameName, preset.ZoneName);
@@ -124,6 +117,22 @@ namespace CollapseLauncher.ShortcutUtils
             string gridPath = Path.Combine(path, "grid");
             if (!Directory.Exists(gridPath))
                 Directory.CreateDirectory(gridPath);
+
+            string iconName = preset.GameType switch
+            {
+                GameType.StarRail => "icon-starrail.ico",
+                GameType.Genshin => "icon-genshin.ico",
+                _ => "icon-honkai.ico",
+            };
+
+            icon = Path.Combine(gridPath, iconName);
+            string iconAssetPath = Path.Combine(Path.GetDirectoryName(AppExecutablePath), "Assets\\Images\\GameIcon\\" + iconName);
+
+            if (!Path.Exists(icon) && Path.Exists(iconAssetPath))
+            {
+                File.Copy(iconAssetPath, icon);
+                LogWriteLine(string.Format("[SteamShortcut::MoveImages] Copied icon from {0} to {1}.", iconAssetPath, icon));
+            }
 
             Dictionary<string, SteamGameProp> assets = preset.ZoneSteamAssets;
 
