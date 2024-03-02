448ff7788ddb497e24118883f0d91366672c18bc
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2024-03-02 11:31:05 +0000

    Enable Rich Presence only after the activated region is set
    might fix #402

--- CollapseLauncher/Program.cs
+++ CollapseLauncher/Program.cs
@@ -69,8 +69,8 @@ namespace CollapseLauncher
 
                 Process.GetCurrentProcess().PriorityBoostEnabled = true;
 
-                InitializeAppSettings();
                 ParseArguments(args);
+                InitializeAppSettings();
 
                 HttpLogInvoker.DownloadLog += HttpClientLogWatcher;
 
@@ -218,10 +218,6 @@ namespace CollapseLauncher
                 CurrentAppTheme = AppThemeMode.Dark;
                 LogWriteLine($"ThemeMode: {themeValue} is invalid! Falling back to Dark-mode (Valid values are: {string.Join(',', Enum.GetNames(typeof(AppThemeMode)))})", LogType.Warning, true);
             }
-#if !DISABLEDISCORD
-            bool isInitialStart = GetAppConfigValue("EnableDiscordRPC").ToBool();
-            AppDiscordPresence = new DiscordPresenceManager(isInitialStart);
-#endif
         }
 
         public static void RunElevateUpdate()
