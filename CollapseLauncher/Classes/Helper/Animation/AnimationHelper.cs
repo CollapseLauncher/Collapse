3b8ed10310a52d2a829ce2a7f70d8f779caec30e
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2024-06-17 14:08:11 +0700

    Update logo + Fix forced update warning layout

--- CollapseLauncher/Classes/Helper/Animation/AnimationHelper.cs
+++ CollapseLauncher/Classes/Helper/Animation/AnimationHelper.cs
@@ -5,7 +5,6 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Hosting;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Animation;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
