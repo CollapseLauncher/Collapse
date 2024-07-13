39e0a9fdf835d80d9182407a9cc3a5f5319aeb3e
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2024-07-13 15:40:33 +0700

    Kod Ki Ei :facepalm:

--- CollapseLauncher/Classes/GameManagement/GameSettings/BaseClass/MagicNodeBaseValues.cs
+++ CollapseLauncher/Classes/GameManagement/GameSettings/BaseClass/MagicNodeBaseValues.cs
@@ -3,7 +3,6 @@ using Hi3Helper;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
