94f9b3e02f7eb91340887b5c461971bdff7c1d93
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-08-30 21:45:39 +0700

    CodeQA Co-authored-by: Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>

--- Hi3Helper.EncTool.Test/Program.cs
+++ Hi3Helper.EncTool.Test/Program.cs
@@ -12,20 +12,20 @@ namespace Hi3Helper.EncTool.Test
             return; //frick testing
             #region unused
 
-            using (SRMetadata srm = new SRMetadata("https://globaldp-prod-os01.starrails.com/query_dispatch", "3a57430d8d",
-                "?version={0}{1}&t={2}&language_type=3&platform_type=3&channel_id=1&sub_channel_id=1&is_new_format=1",
-                "?version={0}{1}&t={2}&uid=0&language_type=3&platform_type=3&dispatch_seed={3}&channel_id=1&sub_channel_id=1&is_need_url=1",
-                "OSPRODWin", "1.5.0"))
-            {
-                await srm.Initialize(default, "prod_official_asia", "F:\\CollapseData\\SRGlb\\Games\\StarRail_Data\\Persistent");
-                await srm.ReadAsbMetadataInformation(default);
-                await srm.ReadBlockMetadataInformation(default);
-                await srm.ReadAudioMetadataInformation(default);
-                await srm.ReadVideoMetadataInformation(default);
-                await srm.ReadIFixMetadataInformation(default);
-                await srm.ReadDesignMetadataInformation(default);
-                await srm.ReadLuaMetadataInformation(default);
-            }
+            // using (SRMetadata srm = new SRMetadata("https://globaldp-prod-os01.starrails.com/query_dispatch", "3a57430d8d",
+            //     "?version={0}{1}&t={2}&language_type=3&platform_type=3&channel_id=1&sub_channel_id=1&is_new_format=1",
+            //     "?version={0}{1}&t={2}&uid=0&language_type=3&platform_type=3&dispatch_seed={3}&channel_id=1&sub_channel_id=1&is_need_url=1",
+            //     "OSPRODWin", "1.5.0"))
+            // {
+            //     await srm.Initialize(default, "prod_official_asia", "F:\\CollapseData\\SRGlb\\Games\\StarRail_Data\\Persistent");
+            //     await srm.ReadAsbMetadataInformation(default);
+            //     await srm.ReadBlockMetadataInformation(default);
+            //     await srm.ReadAudioMetadataInformation(default);
+            //     await srm.ReadVideoMetadataInformation(default);
+            //     await srm.ReadIFixMetadataInformation(default);
+            //     await srm.ReadDesignMetadataInformation(default);
+            //     await srm.ReadLuaMetadataInformation(default);
+            // }
 
             return;
 
@@ -46,14 +46,14 @@ namespace Hi3Helper.EncTool.Test
             string URL = "https://d2wztyirwsuyyo.cloudfront.net/ptpublic/bh3_global/20230109172630_NRBvwa9qTB4EUXIz/extract/BH3_Data/StreamingAssets/Asb/pc/0fae6f42832ea16a84ac496c38437a1d.wmv";
             path = @"C:\Program Files\Honkai Impact 3rd glb\Games\BH3_Data\StreamingAssets\Asb\pc\0fae6f42832ea16a84ac496c38437a1d.wmv";
 
-            using (FileStream fs = new(path, FileMode.Open, FileAccess.Write))
-            {
-                using (ChunkStream cs = new(fs, 0, 457688, false))
-                {
-                    Http.Http client = new(false);
-                    await client.Download(URL, cs, 0, 457688, default, true);
-                }
-            }
+            // using (FileStream fs = new(path, FileMode.Open, FileAccess.Write))
+            // {
+            //     using (ChunkStream cs = new(fs, 0, 457688, false))
+            //     {
+            //         Http.Http client = new(false);
+            //         await client.Download(URL, cs, 0, 457688, default, true);
+            //     }
+            // }
             /*
 
             XMFParser parser = new XMFParser(path);
