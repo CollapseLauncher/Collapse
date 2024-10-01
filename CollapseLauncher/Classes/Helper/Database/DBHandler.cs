3a2560bb776facfafc099d6ac4bf1650a023b16a
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-10-01 21:21:52 +0700

    Fix DBHandler
    - SQL injection fixes
    - Reinitialize DB instance when its stream expired
    - Retry operation when error happens
    - Use UserID hash as the table name

--- CollapseLauncher/Classes/Helper/Database/DBHandler.cs
+++ CollapseLauncher/Classes/Helper/Database/DBHandler.cs
@@ -9,61 +9,67 @@ using System.Threading.Tasks;
 using Windows.System;
 using static Hi3Helper.Logger;
 
-
 namespace CollapseLauncher.Helper.Database
 {
     internal class DbHandler
     {
         #region Config Properties
+
         private static string _uri;
+
         private static string Uri
         {
             get
             {
                 if (!string.IsNullOrEmpty(_uri)) return _uri;
-                
                 var c = DbConfig.DbUrl;
                 _uri = c;
                 return c;
             }
         }
-        
+
         private static string _token;
+
         private static string Token
         {
             get
             {
                 if (!string.IsNullOrEmpty(_token)) return _token;
-                
                 var c = DbConfig.DbToken;
-
                 if (string.IsNullOrEmpty(c)) throw new InvalidDataException("Database token could not be empty!");
                 _token = c;
                 return c;
             }
         }
-        
-        private static Guid _userId;
+
+        private static Guid?  _userId;
+        private static string _userIdHash;
+
         private static Guid UserId
         {
             get
             {
-                if (!string.IsNullOrEmpty(_uri)) return _userId;
-                
+                if (_userId != null) return (Guid)_userId;
                 var c = DbConfig.UserGuid;
                 _userId = c;
+                var byteUidH = System.IO.Hashing.XxHash64.Hash(c.ToByteArray());
+                _userIdHash = BitConverter.ToString(byteUidH).Replace("-", "").ToLowerInvariant();
                 return c;
             }
         }
 
         #endregion
-        
+
         private static IDatabaseClient _database;
 
         public static async void Init()
         {
             DbConfig.Init();
 
+            // Init props
+            _ = Token;
+            _ = Uri;
+            _ = UserId;
             try
             {
                 _database = await DatabaseClient.Create(opts =>
@@ -72,8 +78,8 @@ namespace CollapseLauncher.Helper.Database
                                                             opts.AuthToken = Token;
                                                         });
                 await
-                    _database.Execute("CREATE TABLE IF NOT EXISTS KeyValueDb (Id INTEGER PRIMARY KEY AUTOINCREMENT, 'key' TEXT UNIQUE NOT NULL, 'value' TEXT)");
-                
+                    _database.Execute($"CREATE TABLE IF NOT EXISTS \"uid-{_userIdHash}\" (Id INTEGER PRIMARY KEY AUTOINCREMENT, 'key' TEXT UNIQUE NOT NULL, 'value' TEXT)");
+
                 // test code
                 // await StoreKeyValue("testdate", DateTime.Now.ToString(CultureInfo.CurrentCulture));
                 // var res = await QueryKey("testdate");
@@ -87,10 +93,51 @@ namespace CollapseLauncher.Helper.Database
 
         public static async Task<string> QueryKey(string key)
         {
-            var rs  = await _database.Execute("SELECT value FROM 'KeyValueDb' WHERE key LIKE concat('%', ?, '%')", key);
-            if (rs != null)
+        #if DEBUG
+            LogWriteLine($"[DBHandler::QueryKey] Invoked!\r\n\tKey: {key}", LogType.Debug, true);
+        #endif
+            const int retryCount = 3;
+            for (var i = 0; i < retryCount; i++)
             {
-                return string.Join("", rs.Rows.Select(row => string.Join("", row.Select(x => x.ToString()))));
+                try
+                {
+                    var rs =
+                        await
+                            _database
+                               .Execute($"SELECT value FROM \"uid-{_userIdHash}\" WHERE key LIKE concat('%', ?, '%')",
+                                        key);
+                    if (rs != null)
+                    {
+                        var str =
+                            string.Join("", rs.Rows.Select(row => string.Join("", row.Select(x => x.ToString()))));
+                    #if DEBUG
+                        LogWriteLine($"[DBHandler::QueryKey] Got value!\r\n\tKey: {key}\r\n\t{str}", LogType.Debug,
+                                     true);
+                    #endif
+                        return str;
+                    }
+                }
+                catch (LibsqlException ex) when ((ex.Message.Contains("STREAM_EXPIRED") ||
+                                                  ex.Message.Contains("Received an invalid baton")) &&
+                                                 i < retryCount - 1)
+                {
+                    if (i == 0)
+                        LogWriteLine("[DBHandler::QueryKey] Database stream expired, retrying...", LogType.Error, true);
+
+                    Init();
+                }
+                catch (Exception ex) when (i < retryCount - 1)
+                {
+                    LogWriteLine($"[DBHandler::QueryKey] Failed when getting value for key {key}! Retrying...\r\n{ex}",
+                                 LogType.Error, true);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    LogWriteLine($"[DBHandler::QueryKey] Failed when getting value for key {key} after {retryCount} retries! Returning null...\r\n{ex}",
+                                 LogType.Error, true);
+                    return null;
+                }
             }
 
             return null;
@@ -98,9 +145,43 @@ namespace CollapseLauncher.Helper.Database
 
         public static async Task StoreKeyValue(string key, string value)
         {
-            var command = $"INSERT INTO 'KeyValueDb' (key, value) VALUES ('{key}', '{value}') " +
-                          $"ON CONFLICT(key) DO UPDATE SET value = '{value}'";
-            await _database.Execute(command);
+        #if DEBUG
+            LogWriteLine($"[DBHandler::StoreKeyValue] Invoked!\r\n\tKey: {key}\r\n\tValue: {value}", LogType.Debug,
+                         true);
+        #endif
+            const int retryCount = 5;
+            for (var i = 0; i < retryCount; i++)
+            {
+                try
+                {
+                    var command = $"INSERT INTO \"uid-{_userIdHash}\" (key, value) VALUES (?, ?) " +
+                                  $"ON CONFLICT(key) DO UPDATE SET value = ?";
+                    var parameters = new object[] { key, value, value };
+                    await _database.Execute(command, parameters);
+                }
+                catch (LibsqlException ex) when ((ex.Message.Contains("STREAM_EXPIRED") ||
+                                                 ex.Message.Contains("Received an invalid baton")) &&
+                                                 i < retryCount - 1)
+                {
+                    if (i > 0)
+                        LogWriteLine("[DBHandler::StoreKeyValue] Database stream expired, retrying...", LogType.Error,
+                                     true);
+
+                    Init();
+                }
+                catch (Exception ex) when (i < retryCount - 1)
+                {
+                    LogWriteLine($"[DBHandler::StoreKeyValue] Failed when saving value for key {key}! Retrying...\r\n{ex}",
+                                 LogType.Error, true);
+                }
+                catch (Exception ex)
+                {
+                    LogWriteLine($"[DBHandler::StoreKeyValue] Failed when saving value for key {key} after {retryCount} tries!\r\n{ex}",
+                                 LogType.Error, true);
+                    throw;
+                }
+                
+            }
         }
     }
 }
