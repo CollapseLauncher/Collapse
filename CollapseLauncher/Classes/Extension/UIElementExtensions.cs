26db155463dbaf46552b40eff3fcd00dc6a45fca
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2024-10-05 04:09:42 +0700

    Increase audio language selection opacity

--- CollapseLauncher/Classes/Extension/UIElementExtensions.cs
+++ CollapseLauncher/Classes/Extension/UIElementExtensions.cs
@@ -37,6 +37,17 @@ namespace CollapseLauncher.Extension
         [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_ProtectedCursor")]
         internal static extern void SetCursor(this UIElement element, InputCursor inputCursor);
 
+        /// <summary>
+        /// Set the cursor for the element.
+        /// </summary>
+        /// <param name="element">The <seealso cref="UIElement"/> member of an element</param>
+        /// <param name="inputCursor">The cursor you want to set. Use <see cref="InputSystemCursor.Create"/> to choose the cursor you want to set.</param>
+        internal static ref T WithCursor<T>(this T element, InputCursor inputCursor) where T : UIElement
+        {
+            element.SetCursor(inputCursor);
+            return ref Unsafe.AsRef(ref element);
+        }
+
 #nullable enable
         /// <summary>
         /// Set the initial navigation view item's locale binding before getting set with <seealso cref="ApplyNavigationViewItemLocaleTextBindings"/>
