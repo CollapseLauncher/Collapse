1fda0cec1d4a38ffa21c6366a10615545af39220
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-11-11 23:19:56 +0700

    Documentations

--- Hi3Helper.Core/Classes/SentryHelper/SentryHelper.cs
+++ Hi3Helper.Core/Classes/SentryHelper/SentryHelper.cs
@@ -12,10 +12,33 @@ namespace Hi3Helper.SentryHelper
 {
     public static class SentryHelper
     {
+        /// <summary>
+        /// DSN (Data Source Name a.k.a. upstream server) to be used for error reporting.
+        /// </summary>
+        private const string SentryDsn = "https://2acc39f86f2b4f5a99bac09494af13c6@bugsink.bagelnl.my.id/1";
+        
+        /// <summary>
+        /// <inheritdoc cref="SentryOptions.MaxAttachmentSize"/>
+        /// </summary>
+        private const long SentryMaxAttachmentSize = 2 * 1024 * 1024; // 2 MB
+        
+        
+        /// <summary>
+        /// Defines Exception Types to be used in Sentry's data reporting.
+        /// </summary>
         public enum ExceptionType
         {
+            /// <summary>
+            /// Use this for any unhandled exception that comes from anything Xaml/WinUI related.
+            /// </summary>
             UnhandledXaml,
+            /// <summary>
+            /// Use this method for method that has no meaningful error handling.
+            /// </summary>
             UnhandledOther,
+            /// <summary>
+            /// Use this for exception that is handled directly by the catcher.
+            /// </summary>
             Handled
         }
 
@@ -48,7 +71,7 @@ namespace Hi3Helper.SentryHelper
         {
             _sentryInstance = SentrySdk.Init(o =>
             {
-                o.Dsn = "https://2acc39f86f2b4f5a99bac09494af13c6@bugsink.bagelnl.my.id/1";
+                o.Dsn = SentryDsn;
                 o.AddEventProcessor(new SentryEventProcessor());
 
 #if DEBUG
@@ -72,7 +95,7 @@ namespace Hi3Helper.SentryHelper
 #else
                 o.Distribution = IsPreview ? "Preview" : "Stable";
 #endif
-                o.MaxAttachmentSize = 2 * 1024 * 1024; // 2 MB
+                o.MaxAttachmentSize = SentryMaxAttachmentSize;
                 o.DeduplicateMode = DeduplicateMode.All;
             });
             
@@ -84,6 +107,9 @@ namespace Hi3Helper.SentryHelper
             });
         }
 
+        /// <summary>
+        /// Flush, ends, and dispose SentrySdk instance.
+        /// </summary>
         public static void StopSentrySdk()
         {
             try
@@ -127,8 +153,8 @@ namespace Hi3Helper.SentryHelper
         /// <summary>
         /// Sends exception to Sentry's set DSN
         /// </summary>
-        /// <param name="ex"></param>
-        /// <param name="exT"></param>
+        /// <param name="ex">Exception data</param>
+        /// <param name="exT">Exception type, default to ExceptionType.Handled</param>
         public static void ExceptionHandler(Exception ex, ExceptionType exT = ExceptionType.Handled)
         {
             if (!IsEnabled) return;
@@ -141,6 +167,11 @@ namespace Hi3Helper.SentryHelper
             SentrySdk.Flush(TimeSpan.FromSeconds(5));
         }
 
+        /// <summary>
+        /// <inheritdoc cref="ExceptionHandler"/>
+        /// </summary>
+        /// <param name="ex">Exception data</param>
+        /// <param name="exT">Exception type, default to ExceptionType.Handled</param>
         public static async Task ExceptionHandlerAsync(Exception ex, ExceptionType exT = ExceptionType.Handled)
         {
             if (!IsEnabled) return;
@@ -174,16 +205,27 @@ namespace Hi3Helper.SentryHelper
                     _exHLoopLastEx = null;
                 }
             }
-            catch (TaskCanceledException)
+            catch (Exception)
             {
                 //ignore
             }
         }
 
+        /// <summary>
+        /// Sends exception to Sentry's set DSN. <br/>
+        /// Use this method for any method that may repeat same exceptions multiple time in the time span of 20s.
+        /// </summary>
+        /// <param name="ex">Exception data</param>
+        /// <param name="exT">Exception type, default to ExceptionType.Handled</param>
         public static void ExceptionHandler_ForLoop(Exception ex, ExceptionType exT = ExceptionType.Handled) =>
-            ExceptionHandler_ForLoop_Async(ex, exT).GetAwaiter().GetResult();
+            ExceptionHandler_ForLoopAsync(ex, exT).GetAwaiter().GetResult();
         
-        public static async Task ExceptionHandler_ForLoop_Async(Exception ex, ExceptionType exT = ExceptionType.Handled)
+        /// <summary>
+        /// <inheritdoc cref="ExceptionHandler_ForLoop"/>
+        /// </summary>
+        /// <param name="ex">Exception data</param>
+        /// <param name="exT">Exception type, default to ExceptionType.Handled</param>
+        public static async Task ExceptionHandler_ForLoopAsync(Exception ex, ExceptionType exT = ExceptionType.Handled)
         {
             if (!IsEnabled) return;
             if (ex == _exHLoopLastEx) return;
