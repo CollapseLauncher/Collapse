c8e66309b0ac39b6a27443d283e62787e860818b
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-11-15 23:05:31 +0700

    Improve SentryEventProcessor.cs

--- Hi3Helper.Core/Classes/SentryHelper/SentryEventProcessor.cs
+++ Hi3Helper.Core/Classes/SentryHelper/SentryEventProcessor.cs
@@ -7,9 +7,13 @@ namespace Hi3Helper.SentryHelper
     #nullable enable
     public class SentryEventProcessor : ISentryEventProcessor
     {
+        // This is to follow the default implementation of the interface
+        // ReSharper disable once ReturnTypeCanBeNotNullable
         public SentryEvent? Process(SentryEvent @event)
         {
-            var log = $"Sentry event caught! ID: {@event.EventId}, Level: {@event.Level}\r\n{@event.Message?.Formatted}";
+            var log =
+                $"Sentry event caught! ID: {@event.EventId}, Level: {@event.Level}, Source: {@event.Exception?.Source}" +
+                $"{(string.IsNullOrEmpty(@event.Message?.Formatted) ? "" : $"\r\n{@event.Message?.Formatted}")}";
             LogWriteLine(log, LogType.Sentry, true); // Use our logger
             return @event; // Pipe em back up
         }
