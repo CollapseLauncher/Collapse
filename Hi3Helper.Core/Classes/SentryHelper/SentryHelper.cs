c7c98e502387f3bd259d0021b2d0bdaf08ebcab1
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-11-26 00:26:30 +0700

    [Sentry] Add more breadcrumbs
    Add CPU and GPU data, also use latest user code method for the MechanismKey

--- Hi3Helper.Core/Classes/SentryHelper/SentryHelper.cs
+++ Hi3Helper.Core/Classes/SentryHelper/SentryHelper.cs
@@ -3,12 +3,15 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Hi3Helper.Shared.Region;
+using Microsoft.Win32;
 #if DEBUG
 using Sentry.Infrastructure;
 #endif
 using Sentry.Protocol;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 // ReSharper disable HeuristicUnreachableCode
@@ -16,15 +19,16 @@ using System.Diagnostics;
 #nullable enable
 namespace Hi3Helper.SentryHelper
 {
-    public static class SentryHelper
+    public static partial class SentryHelper
     {
         #region Sentry Configuration
 
         /// <summary>
         /// DSN (Data Source Name a.k.a. upstream server) to be used for error reporting.
         /// </summary>
-        private const string SentryDsn = "https://38df0c6a1a779a4d663def402084187f@o4508297437839360.ingest.de.sentry.io/4508302961410128";
-        
+        private const string SentryDsn =
+            "https://38df0c6a1a779a4d663def402084187f@o4508297437839360.ingest.de.sentry.io/4508302961410128";
+
         /// <summary>
         /// <inheritdoc cref="SentryOptions.MaxAttachmentSize"/>
         /// </summary>
@@ -36,7 +40,7 @@ namespace Hi3Helper.SentryHelper
         private const bool SentryUploadLog = true;
 
         #endregion
-        
+
         #region Enums
 
         /// <summary>
@@ -48,10 +52,12 @@ namespace Hi3Helper.SentryHelper
             /// Use this for any unhandled exception that comes from anything Xaml/WinUI related.
             /// </summary>
             UnhandledXaml,
+
             /// <summary>
             /// Use this method for method that has no meaningful error handling.
             /// </summary>
             UnhandledOther,
+
             /// <summary>
             /// Use this for exception that is handled directly by the catcher.
             /// </summary>
@@ -62,8 +68,11 @@ namespace Hi3Helper.SentryHelper
 
         #region Enable/Disable Sentry
 
-        public static bool IsDisableEnvVarDetected => Convert.ToBoolean(Environment.GetEnvironmentVariable("DISABLE_SENTRY"));
+        public static bool IsDisableEnvVarDetected =>
+            Convert.ToBoolean(Environment.GetEnvironmentVariable("DISABLE_SENTRY"));
+
         private static bool? _isEnabled;
+
         public static bool IsEnabled
         {
             get
@@ -74,6 +83,7 @@ namespace Hi3Helper.SentryHelper
                     LauncherConfig.SetAndSaveConfigValue("SendRemoteCrashData", false);
                     return false;
                 }
+
                 return _isEnabled ??= LauncherConfig.GetAppConfigValue("SendRemoteCrashData").ToBool();
             }
             set
@@ -89,22 +99,22 @@ namespace Hi3Helper.SentryHelper
                 {
                     StopSentrySdk();
                 }
-                
+
                 _isEnabled = value;
                 LauncherConfig.SetAndSaveConfigValue("SendRemoteCrashData", value);
             }
         }
-        
+
         #endregion
-        
+
         #region Initializer/Releaser
 
         public static  bool         IsPreview { get; set; }
         private static IDisposable? _sentryInstance;
-        
+
         public static void InitializeSentrySdk()
         {
-            _sentryInstance = 
+            _sentryInstance =
                 SentrySdk.Init(o =>
                              {
                                  o.Dsn = SentryDsn;
@@ -148,7 +158,7 @@ namespace Hi3Helper.SentryHelper
                                          {
                                              // Do not send user IP address.
                                              // Geolocation should not be uploaded for new users that has not sent any data.
-                                             IpAddress = null 
+                                             IpAddress = null
                                          };
                                      });
         }
@@ -201,7 +211,7 @@ namespace Hi3Helper.SentryHelper
             ex.Data[Mechanism.MechanismKey] = "Application.UnhandledException";
             ExceptionHandler(ex, ExceptionType.UnhandledOther);
         }
-        
+
         private static void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
         {
             e.Exception.Data[Mechanism.HandledKey] = false;
@@ -223,6 +233,7 @@ namespace Hi3Helper.SentryHelper
                                     LogType.Sentry);
                 return;
             }
+
             ExceptionHandlerInner(ex, exT);
         }
 
@@ -241,11 +252,12 @@ namespace Hi3Helper.SentryHelper
                                     LogType.Sentry);
                 return;
             }
+
             ExceptionHandler(ex, exT);
 
             await SentrySdk.FlushAsync(TimeSpan.FromSeconds(10));
         }
-        
+
         private static Exception?              _exHLoopLastEx;
         private static CancellationTokenSource _loopToken = new();
 
@@ -261,7 +273,7 @@ namespace Hi3Helper.SentryHelper
                 var t = _loopToken.Token;
                 await Task.Delay(20000, t);
                 if (_exHLoopLastEx == null) return;
-            
+
                 lock (_exHLoopLastEx)
                 {
                     _exHLoopLastEx = null;
@@ -281,7 +293,7 @@ namespace Hi3Helper.SentryHelper
         /// <param name="exT">Exception type, default to ExceptionType.Handled</param>
         public static void ExceptionHandler_ForLoop(Exception ex, ExceptionType exT = ExceptionType.Handled) =>
             ExceptionHandler_ForLoopAsync(ex, exT).GetAwaiter().GetResult();
-        
+
         /// <summary>
         /// <inheritdoc cref="ExceptionHandler_ForLoop"/>
         /// </summary>
@@ -297,16 +309,18 @@ namespace Hi3Helper.SentryHelper
                                     LogType.Sentry);
                 return;
             }
+
             if (ex == _exHLoopLastEx) return; // If exception pointer is the same as the last one, ignore it.
             await _loopToken.CancelAsync(); // Cancel the previous loop
-            _loopToken.Dispose(); 
+            _loopToken.Dispose();
             _loopToken     = new CancellationTokenSource(); // Create new token
             _exHLoopLastEx = ex;
             ExHLoopLastEx_AutoClean(); // Start auto clean loop
             ExceptionHandlerInner(ex, exT);
         }
 
-        
+        #region Breadcrumbs Data
+
         public static string AppBuildCommit        { get; set; } = "";
         public static string AppBuildBranch        { get; set; } = "";
         public static string AppBuildRepo          { get; set; } = "";
@@ -316,38 +330,153 @@ namespace Hi3Helper.SentryHelper
         public static bool   CurrentGameUpdated    { get; set; }
         public static bool   CurrentGameHasPreload { get; set; }
         public static bool   CurrentGameHasDelta   { get; set; }
+
+        private static int CpuThreadsTotal => Environment.ProcessorCount;
+
+        private static string CpuName
+        {
+            get
+            {
+                try
+                {
+                    string cpuName;
+                    var    env = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER") ?? "Unknown CPU";
+                    var reg =
+                        Registry.GetValue("HKEY_LOCAL_MACHINE\\HARDWARE\\DESCRIPTION\\SYSTEM\\CentralProcessor\\0",
+                                          "ProcessorNameString", null);
+                    if (reg != null)
+                    {
+                        cpuName = reg.ToString() ?? env;
+                        cpuName = cpuName.Trim();
+                    }
+                    else cpuName = env;
+
+                    return cpuName;
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogWriteLine("Failed when trying to get CPU Name!\r\n" + ex, LogType.Error, true);
+                    return "Unknown CPU";
+                }
+            }
+        }
+
+        private static List<(string GpuName, string DriverVersion)> GetGpuInfo
+        {
+            get
+            {
+                List<(string GpuName, string DriverVersion)> gpuInfoList = new();
+                try
+                {
+                    using var baseKey =
+                        Registry.LocalMachine
+                                .OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\");
+                    if (baseKey != null)
+                    {
+                        foreach (var subKeyName in baseKey.GetSubKeyNames())
+                        {
+                            if (!int.TryParse(subKeyName, out int subKeyInt) || subKeyInt < 0 || subKeyInt > 9999)
+                                continue;
+
+                            var gpuName       = "Unknown GPU";
+                            var driverVersion = "Unknown Driver Version";
+                            try
+                            {
+                                using var subKey = baseKey.OpenSubKey(subKeyName);
+                                if (subKey == null) continue;
+
+                                gpuName       = subKey.GetValue("DriverDesc") as string;
+                                driverVersion = subKey.GetValue("DriverVersion") as string;
+                                if (!string.IsNullOrEmpty(gpuName) && !string.IsNullOrEmpty(driverVersion))
+                                {
+                                    gpuInfoList.Add((subKeyName + gpuName, driverVersion));
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                gpuInfoList.Add((subKeyName + gpuName, driverVersion)!);
+                                throw;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogWriteLine($"Failed to retrieve GPU info from registry: {ex.Message}", LogType.Error,
+                                        true);
+                }
+
+                return gpuInfoList;
+            }
+        }
+
+        private static Breadcrumb? _buildInfo;
+        private static Breadcrumb BuildInfo
+        {
+            get => _buildInfo ??= new("Build Info", "commit", new Dictionary<string, string>
+            {
+                { "Branch", AppBuildBranch },
+                { "Commit", AppBuildCommit },
+                { "Repository", AppBuildRepo }
+            }, "BuildInfo");
+        }
+        
+        private static Breadcrumb? _cpuInfo;
+        private static Breadcrumb CpuInfo
+        {
+            get => _cpuInfo ??= new("CPU Info", "system.cpu", new Dictionary<string, string>
+            {
+                { "CPU Name", CpuName },
+                { "Total Thread", CpuThreadsTotal.ToString() }
+            }, "CPUInfo");
+        }
+
+        private static Breadcrumb? _gpuInfo;
+        private static Breadcrumb GpuInfo
+        {
+            get => _gpuInfo ??= new("GPU Info", "system.gpu",
+                                     GetGpuInfo.ToDictionary(item => item.GpuName,
+                                                             item => item.DriverVersion),
+                                     "GPUInfo");
+        }
+        
+        private static Breadcrumb GameInfo =>
+            new("Current Loaded Game Info", "game", new Dictionary<string, string>
+            {
+                { "Category", CurrentGameCategory },
+                { "Region", CurrentGameRegion },
+                { "Installed", CurrentGameInstalled.ToString() },
+                { "Updated", CurrentGameUpdated.ToString() },
+                { "HasPreload", CurrentGameHasPreload.ToString() },
+                { "HasDelta", CurrentGameHasDelta.ToString() }
+            }, "GameInfo");
+
+        #endregion
         
         private static void ExceptionHandlerInner(Exception ex, ExceptionType exT = ExceptionType.Handled)
         {
-            // Breadcrumbs
-            var buildInfo = new Breadcrumb("Build Info", "commit", 
-                                         new Dictionary<string, string>
-                                         {
-                                                        { "Branch", AppBuildBranch },
-                                                        { "Commit", AppBuildCommit },
-                                                        { "Repository", AppBuildRepo }
-                                         }, "BuildInfo");
-            var gameInfo = new Breadcrumb("Current Loaded Game Info", "game",
-                                                   new Dictionary<string, string>
-                                                   {
-                                                        { "Category", CurrentGameCategory },
-                                                        { "Region", CurrentGameRegion },
-                                                        { "Installed", CurrentGameInstalled.ToString() },
-                                                        { "Updated", CurrentGameUpdated.ToString()},
-                                                        { "HasPreload", CurrentGameHasPreload.ToString()},
-                                                        { "HasDelta", CurrentGameHasDelta.ToString()}
-                                                   }, "GameInfo");
-            SentrySdk.AddBreadcrumb(buildInfo);
-            SentrySdk.AddBreadcrumb(gameInfo);
+            SentrySdk.AddBreadcrumb(BuildInfo);
+            SentrySdk.AddBreadcrumb(GameInfo);
+            SentrySdk.AddBreadcrumb(CpuInfo);
+            SentrySdk.AddBreadcrumb(GpuInfo);
             
             ex.Data[Mechanism.HandledKey] ??= exT == ExceptionType.Handled;
-            ex.Data[Mechanism.MechanismKey] = exT switch
+
+            string? methodName = null;
+            var    st = ex.StackTrace;
+            if (st != null)
+            {
+                var          m   = ExceptionFrame().Match(st);
+                methodName = m.Success ? m.Value : null;
+            }
+
+            ex.Data[Mechanism.MechanismKey] ??= exT switch
                                               {
                                                   ExceptionType.UnhandledXaml => "Application.XamlUnhandledException",
-                                                  ExceptionType.UnhandledOther => "Application.UnhandledException",
-                                                  _ => ex.Data[Mechanism.MechanismKey]
+                                                  ExceptionType.UnhandledOther => methodName ?? ex.Source ?? "Application.UnhandledException",
+                                                  _ => methodName ?? ex.Source ?? "Application.HandledException"
                                               };
-            
+
         #pragma warning disable CS0162 // Unreachable code detected
             if (SentryUploadLog) // Upload log file if enabled
                 // ReSharper disable once HeuristicUnreachableCode
@@ -366,6 +495,9 @@ namespace Hi3Helper.SentryHelper
             }
         #pragma warning restore CS0162 // Unreachable code detected
         }
+
+        [GeneratedRegex(@"(?<=\bat\s)(CollapseLauncher|Hi3Helper)\.[^\s(]+", RegexOptions.Compiled)]
+        private static partial Regex ExceptionFrame();
     }
 
         #endregion
