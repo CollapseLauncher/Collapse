0b32e85cd8b40a6b70f40a8518a8ba8314d026d3
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-12-31 08:58:40 +0700

    [Sentry] Use hashset on exception filters

--- Hi3Helper.Core/Classes/SentryHelper/SentryExceptionFilter.cs
+++ Hi3Helper.Core/Classes/SentryHelper/SentryExceptionFilter.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Logging;
 using Sentry.Extensibility;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Sockets;
@@ -9,38 +10,39 @@ namespace Hi3Helper.SentryHelper
 {
     public class NetworkException : IExceptionFilter
     {
+        private static readonly HashSet<int> SocketExceptionFilters =
+        [
+            10050, // WSAENETDOWN - The network subsystem has failed.
+            10051, // WSAENETUNREACH - The network can't be reached from this host at this time.
+            10052, // WSAENETRESET - The connection has been broken due to keep-alive activity detecting a failure while the operation was in progress.
+            10053, // WSAECONNABORTED - The connection was aborted due to a network error.
+            10054, // WSAECONNRESET - The connection was reset by the remote peer.
+            10060, // WSAETIMEDOUT - The connection has been dropped because of a network failure or because the peer system failed to respond.
+            10061, // WSAECONNREFUSED - The connection was refused by the remote host.
+            10065 // WSAEHOSTUNREACH - The host is unreachable.
+        ];
+        
+        private static readonly HashSet<string> HttpExceptionFilters =
+        [
+            "net_http_client_execution_error",
+            "net_http_request_aborted"
+        ];
+        
+        private static readonly HashSet<HttpRequestError> HttpRequestErrorFilters =
+        [
+            HttpRequestError.NameResolutionError,
+            HttpRequestError.SecureConnectionError
+        ];
+
         public bool Filter(Exception ex)
         {
-            var returnValue = false;
-            if (ex is SocketException socketEx)
-            {
-                var filterSocket = new[] {
-                    10050, // WSAENETDOWN - The network subsystem has failed.
-                    10051, // WSAENETUNREACH - The network can't be reached from this host at this time.
-                    10052, // WSAENETRESET - The connection has been broken due to keep-alive activity detecting a failure while the operation was in progress.
-                    10053, // WSAECONNABORTED - The connection was aborted due to a network error.
-                    10054, // WSAECONNRESET - The connection was reset by the remote peer.
-                    10060, // WSAETIMEDOUT - The connection has been dropped because of a network failure or because the peer system failed to respond.
-                    10061, // WSAECONNREFUSED - The connection was refused by the remote host.
-                    10065  // WSAEHOSTUNREACH - The host is unreachable.
-                };
-                
-                returnValue = filterSocket.Any(f => socketEx.ErrorCode == f);
-            }
-            else if (ex is HttpRequestException httpEx)                                                                             
-            {
-                var filter = new[] {
-                    "net_http_client_execution_error",
-                    "net_http_request_aborted"
-                };
-                
-                var filterEnum = new[] {
-                    HttpRequestError.NameResolutionError,
-                    HttpRequestError.SecureConnectionError
-                };
-                
-                returnValue = filterEnum.Any(f => httpEx.HttpRequestError == f) || filter.Any(f => httpEx.Message.Contains(f, StringComparison.InvariantCultureIgnoreCase));
-            }
+            var returnValue = ex switch
+                              {
+                                  SocketException socketEx => SocketExceptionFilters.Contains(socketEx.ErrorCode),
+                                  HttpRequestException httpEx => HttpRequestErrorFilters.Contains(httpEx.HttpRequestError)
+                                                                 || HttpExceptionFilters.Any(f => httpEx.Message.Contains(f)),
+                                  _ => false
+                              };
 
             if (returnValue)
             {
