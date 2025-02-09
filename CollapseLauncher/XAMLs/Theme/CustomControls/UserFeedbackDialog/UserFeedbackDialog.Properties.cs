9fb2d0ca24ea770254ded0526be0119af72e2d86
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-02-09 17:22:08 +0700

    Add docs to the class

--- CollapseLauncher/XAMLs/Theme/CustomControls/UserFeedbackDialog/UserFeedbackDialog.Properties.cs
+++ CollapseLauncher/XAMLs/Theme/CustomControls/UserFeedbackDialog/UserFeedbackDialog.Properties.cs
@@ -33,40 +33,61 @@ namespace CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog
         #endregion
 
         #region Properties
+        /// <summary>
+        /// The predefined title to be set or the result of what has been typed in the
+        /// feedback's <see cref="TextBox"/>.
+        /// </summary>
         public string? Title
         {
             get => (string?)GetValue(TitleProperty);
             set => SetValue(TitleProperty, value);
         }
 
+        /// <summary>
+        /// Set the title's <see cref="TextBox.IsReadOnly"/> property.
+        /// </summary>
         public bool IsTitleReadOnly
         {
             get => (bool?)GetValue(IsTitleReadOnlyProperty) ?? false;
             set => SetValue(IsTitleReadOnlyProperty, value);
         }
 
+        /// <summary>
+        /// Set the visibility of the title's <see cref="TextBox"/>.
+        /// </summary>
         public Visibility TitleVisibility
         {
             get => (Visibility)GetValue(TitleVisibilityProperty);
             set => SetValue(TitleVisibilityProperty, value);
         }
 
+        /// <summary>
+        /// The predefined message to be set or the result of what has been typed in the
+        /// feedback's <see cref="TextBox"/>.
+        /// </summary>
         public string? Message
         {
             get => (string?)GetValue(MessageProperty);
             set => SetValue(MessageProperty, value);
         }
 
+        /// <summary>
+        /// Set the message's <see cref="TextBox.IsReadOnly"/> property.
+        /// </summary>
         public bool IsMessageReadOnly
         {
             get => (bool?)GetValue(IsMessageReadOnlyProperty) ?? false;
             set => SetValue(IsMessageReadOnlyProperty, value);
         }
 
+        /// <summary>
+        /// The predefined rating value to be set or the result of the <see cref="RatingControl"/>.<br/>
+        /// Default: 5, Valid value range: -1 to 5
+        /// </summary>
         public double RatingValue
         {
-            get => (double)GetValue(RatingValueProperty);
-            set => SetValue(RatingValueProperty, value);
+            get => Math.Clamp((double)GetValue(RatingValueProperty), -1, 5);
+            set => SetValue(RatingValueProperty, Math.Clamp(value, -1, 5));
         }
         #endregion
 
@@ -84,18 +105,21 @@ namespace CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog
 
         private void OnSubmissionSubmitButtonClick(object sender, RoutedEventArgs e)
         {
+            // Set submission as true and invalidate token source
             _isSubmit = true;
             InvalidateTokenSource();
         }
 
         private void OnSubmissionCancelButtonClick(object sender, RoutedEventArgs e)
         {
+            // Set submission as false and invalidate token source
             _isSubmit = false;
             InvalidateTokenSource();
         }
 
         private async Task OnRunningSubmitTask(Func<UserFeedbackResult?, CancellationToken, Task>? actionCallbackTaskOnSubmit, UserFeedbackResult? result)
         {
+            // If the callback task is null or is not submitted, return
             if (!(actionCallbackTaskOnSubmit is not null && _isSubmit))
             {
                 return;
@@ -103,8 +127,11 @@ namespace CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog
 
             try
             {
+                // Assign the token source and set the UI state of tbe Submit button
                 _currentConfirmTokenSource ??= new CancellationTokenSource();
                 OnSubmitBtnRunningTask();
+
+                // Call the callback task and pass the result and cancellation token
                 await actionCallbackTaskOnSubmit(result, _currentConfirmTokenSource.Token);
             }
             catch
@@ -113,6 +140,8 @@ namespace CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog
             }
             finally
             {
+                // Once all have been executed, set the UI state of the Submit button as completed
+                // and dispose the token source.
                 await OnSubmitBtnCompletedTask();
                 DisposeTokenSource();
             }
@@ -120,21 +149,25 @@ namespace CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog
 
         private void OnSubmitBtnRunningTask()
         {
+            // If the UI buttons and input control is null, return
             if (!(_layoutPrimaryButton is not null &&
                   _layoutFeedbackTitleInput is not null &&
                   _layoutFeedbackMessageInput is not null &&
                   _layoutFeedbackRatingControl is not null))
                 return;
 
+            // Find the UI element where each of its state to be changed
             TextBlock? textBlock = (_layoutPrimaryButton.Content as UIElement)?.FindDescendant<TextBlock>();
             ProgressRing? progressRing = (_layoutPrimaryButton.Content as UIElement)?.FindDescendant<ProgressRing>();
             Grid? iconGrid = (_layoutPrimaryButton.Content as UIElement)?.FindDescendant<Grid>();
 
+            // If any of them is null, return
             if (textBlock is null ||
                 progressRing is null ||
                 iconGrid is null)
                 return;
 
+            // Set the UI element state and disable some elements (to prevent the result's value to be changed).
             _layoutPrimaryButton.IsEnabled         = false;
             _layoutFeedbackTitleInput.IsEnabled    = false;
             _layoutFeedbackMessageInput.IsEnabled  = false;
@@ -144,6 +177,7 @@ namespace CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog
             progressRing.Opacity                   = 1d;
             iconGrid.Opacity                       = 0d;
 
+            // Set the text as "Processing..." to the Submit button.
             textBlock.BindProperty(TextBlock.TextProperty,
                                    Locale.Lang._Dialogs,
                                    nameof(Locale.Lang._Dialogs.UserFeedback_SubmitBtn_Processing));
@@ -151,29 +185,36 @@ namespace CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog
 
         private Task OnSubmitBtnCompletedTask()
         {
+            // If any of the button is null, return
             if (!(_layoutPrimaryButton is not null &&
                   _layoutCloseButton is not null))
                 return Task.CompletedTask;
 
+            // Find the UI element where each of its state to be changed and set the cancel state
             TextBlock? textBlock = (_layoutPrimaryButton.Content as UIElement)?.FindDescendant<TextBlock>();
             ProgressRing? progressRing = (_layoutPrimaryButton.Content as UIElement)?.FindDescendant<ProgressRing>();
             bool isCancelled = _currentConfirmTokenSource?.IsCancellationRequested ?? false;
 
+            // If any of the UI element is missing, return
             if (textBlock is null ||
                 progressRing is null)
                 return Task.CompletedTask;
 
+            // Change the UI state and disable the close/cancel button
             progressRing.IsIndeterminate = false;
             _layoutCloseButton.IsEnabled = false;
 
+            // Set the Submit button's text based on the isCancelled state.
             textBlock.BindProperty(TextBlock.TextProperty,
                                    Locale.Lang._Dialogs,
                                    isCancelled ?
                                        nameof(Locale.Lang._Dialogs.UserFeedback_SubmitBtn_Cancelled) :
                                        nameof(Locale.Lang._Dialogs.UserFeedback_SubmitBtn_Completed));
 
+            // Delay before return
             return ReturnDelay();
 
+            // Return for 1 second
             Task ReturnDelay() => Task.Delay(TimeSpan.FromSeconds(1));
         }
 
@@ -182,6 +223,7 @@ namespace CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog
 
         private void DisposeTokenSource()
         {
+            // Dispose the token source and set the token source as null.
             _currentConfirmTokenSource?.Dispose();
             Interlocked.Exchange(ref _currentConfirmTokenSource, null);
         }
