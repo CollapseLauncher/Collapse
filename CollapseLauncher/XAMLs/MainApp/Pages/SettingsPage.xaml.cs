de466b97aec365a9f9bc304561bea9b0cbe4f84c
Bagus Nur Listiyono <dzakibagus@gmail.com>
2025-03-11 12:52:00 +0700

    User feedback UX improvements 1. Disable generic feedback when Sentry is disabled 2. Add loading pop up when sending feedback for UX 3. Add simple error message in the loading pop up for UX

--- CollapseLauncher/XAMLs/MainApp/Pages/SettingsPage.xaml.cs
+++ CollapseLauncher/XAMLs/MainApp/Pages/SettingsPage.xaml.cs
@@ -14,6 +14,7 @@ using CollapseLauncher.Helper.Update;
 using CollapseLauncher.Pages.OOBE;
 using CollapseLauncher.Statics;
 #if ENABLEUSERFEEDBACK
+using CollapseLauncher.Helper.Loading;
 using CollapseLauncher.XAMLs.Theme.CustomControls.UserFeedbackDialog;
 #endif
 using CommunityToolkit.WinUI;
@@ -130,6 +131,8 @@ namespace CollapseLauncher.Pages
 
 #if !ENABLEUSERFEEDBACK
             ShareYourFeedbackButton.Visibility = Visibility.Collapsed;
+#else
+            ShareYourFeedbackButton.IsEnabled = SentryHelper.IsEnabled;
 #endif
         }
 
@@ -437,15 +440,38 @@ namespace CollapseLauncher.Pages
                 return;
             }
 
-            var parsedFeedback = UserFeedbackTemplate.ParseTemplate(userFeedbackResult);
+            var parsedFeedback       = UserFeedbackTemplate.ParseTemplate(userFeedbackResult);
+            var feedbackLoadingTitle = Lang._Misc.Feedback;
+            
+            // Show pseudo-loading message so user knows the feedback is being sent
+            LoadingMessageHelper.Initialize();
+            LoadingMessageHelper.SetMessage(feedbackLoadingTitle, Lang._Misc.FeedbackSending);
+            LoadingMessageHelper.ShowLoadingFrame();
             
             if (parsedFeedback == null)
             {
                 LogWriteLine("Feedback result failed to be parsed! Feedback not sent.", LogType.Error, true);
+                LoadingMessageHelper.SetMessage(feedbackLoadingTitle, Lang._Misc.FeedbackSendFailure);
+                await Task.Delay(1000);
+                LoadingMessageHelper.HideLoadingFrame();
                 return;
             }
 
-            SentryHelper.SendGenericFeedback(parsedFeedback.Message, parsedFeedback.Email, parsedFeedback.User);
+            if (SentryHelper.SendGenericFeedback(parsedFeedback.Message, parsedFeedback.Email, parsedFeedback.User))
+            {
+                // Hide the loading message after 200ms
+                await Task.Delay(500);
+                LoadingMessageHelper.SetMessage(feedbackLoadingTitle, Lang._Misc.FeedbackSent);
+                await Task.Delay(1000);
+                LoadingMessageHelper.HideLoadingFrame();
+            }
+            else
+            {
+                await Task.Delay(250);
+                LoadingMessageHelper.SetMessage(feedbackLoadingTitle, Lang._Misc.FeedbackSendFailure);
+                await Task.Delay(1000);
+                LoadingMessageHelper.HideLoadingFrame();
+            }
         #endif
         }
 
