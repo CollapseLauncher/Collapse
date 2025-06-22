52923ebcaefbeb8bac612d4d62be068c8ac14754
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-06-22 21:08:30 +0700

    Fix cast exception while performing dispose.
    Use Free() provided by IFree interface instead.

--- CollapseLauncher/Plugins/PluginGameInstallWrapper.cs
+++ CollapseLauncher/Plugins/PluginGameInstallWrapper.cs
@@ -66,7 +66,7 @@ internal class PluginGameInstallWrapper : ProgressBase<PkgVersionProperties>, IG
 
     public void Dispose()
     {
-        _gameInstaller.Dispose();
+        _gameInstaller.Free();
         GC.SuppressFinalize(this);
     }
 
