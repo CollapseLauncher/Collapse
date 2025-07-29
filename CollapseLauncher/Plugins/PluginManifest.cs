e0254b65cd7e577ad37256a22032e14c136354c4
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-07-30 00:37:33 +0700

    Update PluginManifest + Plugin.Core

--- CollapseLauncher/Plugins/PluginManifest.cs
+++ CollapseLauncher/Plugins/PluginManifest.cs
@@ -1,6 +1,7 @@
 ﻿using Hi3Helper.Plugin.Core.Management;
 using Hi3Helper.Plugin.Core.Utility.Json.Converters;
 using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace CollapseLauncher.Plugins;
@@ -13,16 +14,27 @@ public class PluginManifest
 {
     public required string  MainLibraryName       { get; init; }
     public required string  MainPluginName        { get; init; }
-    public required string  MainPluginAuthor      { get; init; }
+    public          string? MainPluginAuthor      { get; init; }
     public          string? MainPluginDescription { get; init; }
 
     [JsonConverter(typeof(Utf8SpanParsableJsonConverter<GameVersion>))]
     public GameVersion PluginStandardVersion { get; init; }
 
     [JsonConverter(typeof(Utf8SpanParsableJsonConverter<GameVersion>))]
-    public GameVersion PluginVersion         { get; init; }
+    public GameVersion PluginVersion { get; init; }
 
     [JsonConverter(typeof(Utf16SpanParsableJsonConverter<DateTimeOffset>))]
-    public DateTimeOffset PluginCreationDate    { get; init; }
-    public DateTimeOffset ManifestDate          { get; init; }
+    public DateTimeOffset PluginCreationDate { get; init; }
+
+    [JsonConverter(typeof(Utf16SpanParsableJsonConverter<DateTimeOffset>))]
+    public DateTimeOffset ManifestDate { get; init; }
+
+    public required List<PluginManifestAsset> Assets { get; init; }
+}
+
+public class PluginManifestAsset
+{
+    public          string? FilePath { get; set; }
+    public          long    Size     { get; set; }
+    public required byte[]  FileHash { get; set; }
 }
