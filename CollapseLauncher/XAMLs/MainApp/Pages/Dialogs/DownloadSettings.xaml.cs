017e48802aef5ab1c6eb8a3700754caafb9216ad
Gabriel Lima <44784408+gablm@users.noreply.github.com>
2025-08-15 23:13:00 +0100

    Remove unused usings

--- CollapseLauncher/XAMLs/MainApp/Pages/Dialogs/DownloadSettings.xaml.cs
+++ CollapseLauncher/XAMLs/MainApp/Pages/Dialogs/DownloadSettings.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CollapseLauncher.Helper.Animation;
-using Hi3Helper.Shared.Region;
+﻿using Hi3Helper.Shared.Region;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
