87cd3c1c5547d1030402822202f7de6c12c2fd69
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-11-28 21:07:48 +0700

    Initial commit for BackgroundTest project

---
+++ BackgroundTest/App.xaml.cs
@@ -0,0 +1,23 @@
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+// ReSharper disable SwitchStatementMissingSomeEnumCasesNoDefault
+// ReSharper disable CommentTypo
+// ReSharper disable StringLiteralTypo
+
+namespace BackgroundTest;
+
+public partial class App
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+    }
+
+    public static void ToggleBlurBackdrop(bool useBackdrop = true)
+    {
+    }
+}
---
+++ BackgroundTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using PhotoSauce.MagicScaler;
+using PhotoSauce.NativeCodecs.Libheif;
+using PhotoSauce.NativeCodecs.Libjxl;
+using PhotoSauce.NativeCodecs.Libwebp;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace BackgroundTest;
+
+public class MainEntryPoint
+{
+    private static App? _currentApp;
+
+    [STAThread]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Main(params string[] args)
+    {
+        // Initialize support for MagicScaler's WebP decoding
+        CodecManager.Configure(InitializeMagicScalerCodecs);
+        Application.Start(InitializeApp);
+    }
+
+    private static void InitializeMagicScalerCodecs(CodecCollection codecs)
+    {
+        codecs.UseLibwebp();
+        codecs.UseLibheif();
+        codecs.UseLibjxl();
+        codecs.UseWicCodecs(WicCodecPolicy.All);
+    }
+
+    private static void InitializeApp(ApplicationInitializationCallbackParams pContext)
+    {
+        DispatcherQueue dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+
+        DispatcherQueueSynchronizationContext context = new(dispatcherQueue);
+        SynchronizationContext.SetSynchronizationContext(context);
+
+        // ReSharper disable once ObjectCreationAsStatement
+        _currentApp ??= new App
+        {
+            HighContrastAdjustment = ApplicationHighContrastAdjustment.None
+        };
+    }
+}
