f9016e501a00184efce39511bae9166d026acbac
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2025-12-05 14:58:52 +0700

    Initial property for MediaSlideshow

---
+++ BackgroundTest/CustomControl/MediaSlideshow/MediaSlideshow.Events.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackgroundTest.CustomControl.MediaSlideshow;
+
+public partial class MediaSlideshow
+{
+    #region Events
+
+    private static void MediaItems_OnChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
---
+++ BackgroundTest/CustomControl/MediaSlideshow/MediaSlideshow.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace BackgroundTest.CustomControl.MediaSlideshow;
+
+public partial class MediaSlideshow : Control;
