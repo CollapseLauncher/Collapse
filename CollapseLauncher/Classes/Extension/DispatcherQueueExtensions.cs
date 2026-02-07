7302c7ffcaa67e50bfec634c0e2052f286e04152
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-02-08 02:54:11 +0700

    Fix double call on DispatcherQueueExtensions.TryEnqueue

--- CollapseLauncher/Classes/Extension/DispatcherQueueExtensions.cs
+++ CollapseLauncher/Classes/Extension/DispatcherQueueExtensions.cs
@@ -69,6 +69,7 @@ public static class DispatcherQueueExtensions
         if (HasThreadAccessSafe())
         {
             action();
+            return;
         }
 
         CurrentDispatcherQueue.TryEnqueue(priority, action);
