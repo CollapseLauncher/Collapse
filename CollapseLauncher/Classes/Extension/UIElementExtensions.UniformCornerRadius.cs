fd91c991c64b197a7b3385a50c428fe96a60dab3
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-02-28 21:17:15 +0700

    Minor adjustment to Game Region Change button + Accept Panels on UniformCornerRadius

--- CollapseLauncher/Classes/Extension/UIElementExtensions.UniformCornerRadius.cs
+++ CollapseLauncher/Classes/Extension/UIElementExtensions.UniformCornerRadius.cs
@@ -10,44 +10,46 @@ namespace CollapseLauncher.Extension;
 
 public static partial class UIElementExtensions
 {
+    private static readonly Size ZeroSize = default;
+
     /// <summary>
     /// Sets uniform corner radius size. If value is set to -1, the rounded corner radius will be applied.
     /// </summary>
     public static readonly DependencyProperty UniformCornerRadiusProperty =
         DependencyProperty.RegisterAttached("UniformCornerRadius", typeof(double),
-                                            typeof(Control), new PropertyMetadata(0));
+                                            typeof(FrameworkElement), new PropertyMetadata(0));
 
     public static double GetUniformCornerRadius(DependencyObject obj) => obj.GetValue(CursorTypeProperty).TryGetDouble();
 
     public static void SetUniformCornerRadius(DependencyObject obj, double value)
     {
-        if (obj is not Control element)
+        if (obj is not FrameworkElement asElement)
         {
             return;
         }
 
-        UnsetUniformCornerRadiusEvent(element);
+        UnsetUniformCornerRadiusEvent(asElement);
         if (value < 0)
         {
-            ApplyUniformCornerRadiusSize(element, element.RenderSize);
-            SetUniformCornerRadiusEvent(element);
+            ApplyUniformCornerRadiusSize(asElement, asElement.RenderSize);
+            SetUniformCornerRadiusEvent(asElement);
             return;
         }
 
-        element.CornerRadius = new CornerRadius(value);
+        ApplyUniformCornerRadiusSize(asElement, new CornerRadius(value));
     }
 
-    private static void UnsetUniformCornerRadiusEvent(Control element)
+    private static void UnsetUniformCornerRadiusEvent(FrameworkElement element)
     {
         element.SizeChanged -= UniformCornerRadius_ElementOnSizeChanged;
     }
 
-    private static void SetUniformCornerRadiusEvent(Control element)
+    private static void SetUniformCornerRadiusEvent(FrameworkElement element)
     {
         element.SizeChanged += UniformCornerRadius_ElementOnSizeChanged;
     }
 
-    private static void ApplyUniformCornerRadiusSize(Control element, Size size)
+    private static void ApplyUniformCornerRadiusSize(FrameworkElement element, Size size)
     {
         double preferredSize = size.Width < size.Height
             ? size.Width
@@ -57,14 +59,29 @@ public static partial class UIElementExtensions
         {
             preferredSize = 0;
         }
-        element.CornerRadius = new CornerRadius(preferredSize / 2);
+
+        ApplyUniformCornerRadiusSize(element, new CornerRadius(preferredSize / 2));
     }
 
-    private static readonly Size ZeroSize = default;
+    private static void ApplyUniformCornerRadiusSize(FrameworkElement element, CornerRadius cornerRadius)
+    {
+        switch (element)
+        {
+            case Control asControl:
+                asControl.CornerRadius = cornerRadius;
+                break;
+            case StackPanel asStackPanel:
+                asStackPanel.CornerRadius = cornerRadius;
+                break;
+            case Grid asGrid:
+                asGrid.CornerRadius = cornerRadius;
+                break;
+        }
+    }
 
     private static void UniformCornerRadius_ElementOnSizeChanged(object sender, SizeChangedEventArgs e)
     {
-        if (sender is not Control element ||
+        if (sender is not FrameworkElement element ||
             e.NewSize == ZeroSize)
         {
             return;
