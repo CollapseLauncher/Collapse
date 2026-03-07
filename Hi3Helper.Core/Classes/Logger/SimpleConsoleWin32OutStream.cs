41350ae6fe73664c467034bbab0f6d7ee55e0758
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-03-08 01:40:25 +0700

    Ditch unsafe code in SimpleConsoleWin32OutStream

--- Hi3Helper.Core/Classes/Logger/SimpleConsoleWin32OutStream.cs
+++ Hi3Helper.Core/Classes/Logger/SimpleConsoleWin32OutStream.cs
@@ -9,25 +9,26 @@ namespace Hi3Helper;
 
 internal sealed class SimpleConsoleWin32OutStream : Stream
 {
-    private readonly unsafe void* _consoleHandleUnsafe;
-    private readonly        bool  _isFreeConsoleHandle;
+    private readonly nint _consoleHandleUnsafe;
+    private readonly bool _isFreeConsoleHandle;
 
-    internal unsafe SimpleConsoleWin32OutStream(nint consoleHandle, bool freeConsoleHandle = false)
+    internal SimpleConsoleWin32OutStream(nint consoleHandle, bool freeConsoleHandle = false)
     {
-        _consoleHandleUnsafe = (void*)consoleHandle;
+        _consoleHandleUnsafe = consoleHandle;
         _isFreeConsoleHandle = freeConsoleHandle;
     }
 
     [DllImport("Kernel32.dll", EntryPoint = "WriteConsoleA")]
-    private static extern unsafe int WriteConsole(void* hConsoleOutput,
-                                                  void* lpBuffer,
-                                                  uint  nNumberOfCharsToWrite,
-                                                  uint* lpNumberOfCharsWritten,
-                                                  void* lpReserved);
+    private static extern int WriteConsole(nint     hConsoleOutput,
+                                           ref byte lpBuffer,
+                                           int      nNumberOfCharsToWrite,
+                                           ref int  lpNumberOfCharsWritten,
+                                           nint     lpReserved);
 
     [DllImport("Kernel32.dll", EntryPoint = "CloseHandle")]
-    private static extern unsafe int CloseHandle(void* handle);
+    private static extern int CloseHandle(nint handle);
 
+    [SkipLocalsInit]
     public override void Flush()
     {
         // NOP: Intended. Reason: Console doesn't have flush function.
@@ -39,34 +40,37 @@ internal sealed class SimpleConsoleWin32OutStream : Stream
 
     public override void SetLength(long value) => throw new NotSupportedException();
 
+    [SkipLocalsInit]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public override void Write(byte[] buffer, int offset, int count)
         => Write(buffer.AsSpan(offset, count));
 
+    [SkipLocalsInit]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public override unsafe void Write(ReadOnlySpan<byte> buffer)
+    public override void Write(ReadOnlySpan<byte> buffer)
     {
-        void* ptr = Unsafe.AsPointer(ref MemoryMarshal.GetReference(buffer));
-        WriteCore(ptr, (uint)buffer.Length);
+        ref byte bufferRef = ref MemoryMarshal.GetReference(buffer);
+        WriteCore(ref bufferRef, buffer.Length);
     }
 
+    [SkipLocalsInit]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe void WriteCore(void* buffer, uint len)
+    public void WriteCore(ref byte buffer, int len)
     {
-        uint written = 0;
+        int written = 0;
 
-        Write:
-        int result = WriteConsole(_consoleHandleUnsafe, buffer, len, &written, null);
+    Write:
+        int result = WriteConsole(_consoleHandleUnsafe, ref buffer, len, ref written, nint.Zero);
         Marshal.ThrowExceptionForHR(result);
         len -= written;
         if (len > 0)
         {
-            buffer = Unsafe.Add<byte>(buffer, (int)written);
+            buffer = ref Unsafe.Add(ref buffer, written);
             goto Write;
         }
     }
 
-    protected override unsafe void Dispose(bool disposing)
+    protected override void Dispose(bool disposing)
     {
         if (!disposing || !_isFreeConsoleHandle)
         {
