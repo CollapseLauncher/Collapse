5980caa9f229dce4de48a2e3c1b189eca428880e
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-03-15 09:08:19 +0700

    Small code QA on GamePresetProperty

--- CollapseLauncher/Classes/GamePresetProperty.cs
+++ CollapseLauncher/Classes/GamePresetProperty.cs
@@ -107,29 +107,17 @@ namespace CollapseLauncher
         internal IGameVersion?        GameVersion    { get; private set; }
         internal IGameInstallManager? GameInstall    { get; private set; }
         internal WpfPackageContext?   GameWpfContext { get; private set; }
-        internal PresetConfig         GamePreset     { get => GameVersion?.GamePreset ?? throw new NullReferenceException(); }
+        internal PresetConfig         GamePreset     => GameVersion?.GamePreset ?? throw new NullReferenceException();
 
         private ILogger? GamePropLogger { get; set; }
 
-        internal string GameExecutableName
-        {
-            get => GamePreset.GameExecutableName ?? "";
-        }
+        internal string GameExecutableName => GamePreset.GameExecutableName ?? "";
 
-        internal string GameExecutableNameWithoutExtension
-        {
-            get => Path.GetFileNameWithoutExtension(GameExecutableName);
-        }
+        internal string GameExecutableNameWithoutExtension => Path.GetFileNameWithoutExtension(GameExecutableName);
 
-        private string GameExecutableDir
-        {
-            get => GameVersion?.GameDirPath ?? throw new NullReferenceException();
-        }
+        private string GameExecutableDir => GameVersion?.GameDirPath ?? throw new NullReferenceException();
 
-        private string GameExecutablePath
-        {
-            get => Path.Combine(GameExecutableDir, GameExecutableName);
-        }
+        private string GameExecutablePath => Path.Combine(GameExecutableDir, GameExecutableName);
 
         internal bool IsGameRunning
         {
