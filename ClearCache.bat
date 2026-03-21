7e11b2e64000712a0ac2c8e68dc7bad3013de179
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2026-03-21 22:35:31 +0700

    Update ClearCache.bat

--- ClearCache.bat
+++ ClearCache.bat
@@ -1,33 +1,31 @@
 @echo off
-echo 	Clearing Collapse cache
+echo     Clearing Collapse cache
 rmdir /S /Q CollapseLauncher\bin && rmdir /S /Q CollapseLauncher\obj
-echo 	Clearing ColorThief cache
+echo     Clearing ColorThief cache
 rmdir /S /Q ColorThief\ColorThief\bin && rmdir /S /Q ColorThief\ColorThief\obj
-echo 	Clearing CommunityToolkit.ImageCropper cache
+echo     Clearing CommunityToolkit.ImageCropper cache
 rmdir /S /Q Hi3Helper.CommunityToolkit\ImageCropper\bin && rmdir /S /Q Hi3Helper.CommunityToolkit\ImageCropper\obj
-echo 	Clearing CommunityToolkit.SettingsControls cache
+echo     Clearing CommunityToolkit.SettingsControls cache
 rmdir /S /Q Hi3Helper.CommunityToolkit\SettingsControls\bin && rmdir /S /Q Hi3Helper.CommunityToolkit\SettingsControls\obj
-echo 	Clearing Core cache
+echo     Clearing Core cache
 rmdir /S /Q Hi3Helper.Core\bin && rmdir /S /Q Hi3Helper.Core\obj
-echo 	Clearing EncTool cache
+echo     Clearing EncTool cache
 rmdir /S /Q Hi3Helper.EncTool\bin && rmdir /S /Q Hi3Helper.EncTool\obj
-echo 	Clearing EncTool tester cache
-rmdir /S /Q Hi3Helper.EncTool.Test\bin && rmdir /S /Q Hi3Helper.EncTool.Test\obj
-echo 	Clearing Http cache
+echo     Clearing Http cache
 rmdir /S /Q Hi3Helper.Http\bin && rmdir /S /Q Hi3Helper.Http\obj
-echo 	Clearing Http tester cache
-rmdir /S /Q Hi3Helper.Http\Test\bin && rmdir /S /Q Hi3Helper.Http\Test\obj
-echo 	Clearing TaskScheduler cache
+echo     Clearing TaskScheduler cache
 rmdir /S /Q Hi3Helper.TaskScheduler\bin && rmdir /S /Q Hi3Helper.TaskScheduler\obj
-echo 	Clearing HDiff cache
-rmdir /S /Q Hi3Helper.SharpHDiffPatch\Hi3Helper.SharpHDiffPatch\bin && rmdir /S /Q Hi3Helper.SharpHDiffPatch\Hi3Helper.SharpHDiffPatch\obj
-echo 	Clearing 2nd HDiff cache
-rmdir /S /Q Hi3Helper.SharpHDiffPatch\SharpHDiffPatch\bin && rmdir /S /Q Hi3Helper.SharpHDiffPatch\SharpHDiffPatch\obj
-echo 	Clearing InnoSetupHelper cache
+echo     Clearing InnoSetupHelper cache
 rmdir /S /Q InnoSetupHelper\bin && rmdir /S /Q InnoSetupHelper\obj
-echo 	Clearing ImageEx cache
+echo     Clearing ImageEx cache
 rmdir /S /Q ImageEx\ImageEx\bin && rmdir /S /Q ImageEx\ImageEx\obj
-echo 	Clearing 7z cache
-rmdir /S /Q Hi3Helper.Core\Classes\Data\Tools\SevenZipTool\SevenZipExtractor\SevenZipExtractor\bin && rmdir /S /Q Hi3Helper.Core\Classes\Data\Tools\SevenZipTool\SevenZipExtractor\SevenZipExtractor\obj
-echo 	Clearing SharpDiscordRPC cache
+echo     Clearing 7z cache
+rmdir /S /Q SevenZipExtractor\SevenZipExtractor\bin && rmdir /S /Q SevenZipExtractor\SevenZipExtractor\obj
+echo     Clearing SharpDiscordRPC cache
 rmdir /S /Q Hi3Helper.SharpDiscordRPC\DiscordRPC\bin && rmdir /S /Q Hi3Helper.SharpDiscordRPC\DiscordRPC\obj
+echo     Clearing Hi3Helper.SourceGen cache
+rmdir /S /Q Hi3Helper.SourceGen\bin && rmdir /S /Q Hi3Helper.SourceGen\obj
+echo     Clearing Hi3Helper.LocaleSourceGen cache
+rmdir /S /Q Hi3Helper.LocaleSourceGen\bin && rmdir /S /Q Hi3Helper.LocaleSourceGen\obj
+echo     Clearing Hi3Helper.Plugin.Core cache
+rmdir /S /Q Hi3Helper.Plugin.Core\bin && rmdir /S /Q Hi3Helper.Plugin.Core\obj
